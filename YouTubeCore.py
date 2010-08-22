22758b54f6e59169eaf98b0682b38f2a93da95e5
develop@tobiasussing.dk <develop@tobiasussing.dk>
2010-08-22 22:52:41 +0000

    Added more exceptions to login5
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%4046

--- YouTubeCore.py
+++ YouTubeCore.py
@@ -84,17 +84,17 @@ class YouTubeCore(object):
 				print self.__plugin__ + " login failed, hit valueerror except: " + error
 			return ( error, 303 )
 		
-		except urllib2.URLError, e:
-			error = repr(e)
-			if self.__dbg__:
-				print self.__plugin__ + " login failed, hit url except: " + error
-			return ( error, 303 )
-			
 		except IOError, e:
 			error = repr(e)
 			if self.__dbg__:
 				print self.__plugin__ + " login failed, hit ioerror except: " + error
 			return ( error, 303 )
+		
+		except urllib2.URLError, e:
+			error = repr(e)
+			if self.__dbg__:
+				print self.__plugin__ + " login failed, hit url except: " + error
+			return ( error, 303 )										
 		except:
 			if self.__dbg__:
 				print self.__plugin__ + " login failed uncaught exception"
