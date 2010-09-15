7cace80d89a1c185136836e25353f86283774517
Commander.John.Crichton <none@none>
2010-09-15 01:02:37 +0000

    (fixed) accidentally broke trailers scraper during testing (added) basic disco scraper only returns video id's
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%40173

--- YouTubeScraperCore.py
+++ YouTubeScraperCore.py
@@ -36,10 +36,43 @@ class YouTubeScraperCore:
                     item["Title"] = trailer.div.a.span.img['title']
 
                     vobjects.append(item)
-                trailer = trailer.findNextSibling(name="div", attrs = { 'class':"trailer-cell *v1" })
+                trailer = trailer.findNextSibling(name="div", attrs = { 'class':"trailer-cell *vl" })
         
         print repr(vobjects)
                 
+        return vobjects
+    
+    def _searchDisco(self, query, params = {}):
+        get = params.get
+        vobjects = []
+        main_url = "http://www.youtube.com"
+        url = main_url + "/disco?action_search=1&query=" + query
+        page = scraper._fetchPage(url)
+        if (page.find("watch?") != -1):
+            page = page[page.find("/watch?"):page.rfind('"')]
+            url = "http://www.youtube.com" + page
+            page = scraper._fetchPage(url)
+            
+            list = SoupStrainer(id="quicklist", name="div")
+            ajax = BeautifulSoup(page, parseOnlyThese=list)
+            if (len(ajax) > 0):
+                url = main_url + ajax.div["data-active-ajax-url"]
+                page = scraper._fetchPage(url)
+    
+                video_list = SoupStrainer(name="ol")
+                videos = BeautifulSoup(page, parseOnlyThese=video_list)
+                if (len(videos) > 0):
+                    video = videos.li
+                    while (video != None):
+                        item = {}
+                        videoid = video.a["href"]
+                        if (videoid.find("=") > 0):
+                            videoid = videoid[videoid.find("=") +1:videoid.find("&")]
+                        item["videoid"] = videoid
+                        vobjects.append(item)
+                        video = video.findNextSibling(name="li", attrs = { 'class':re.compile("^quicklist-item")})
+        
+        print repr(vobjects)
         return vobjects
         
     def scrapeTrailersListFormat (self, page, params = {}):
@@ -107,5 +140,9 @@ if __name__ == '__main__':
     page = scraper._fetchPage(url)
     scraper.scrapeTrailersListFormat(page)
 
+    print " " 
+    search_query = "Rihanna"
+    print "Disco Search for " + search_query 
+    scraper._searchDisco(search_query)
 
     sys.exit(0);
