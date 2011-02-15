936afc8d3d65bbed87414a89a32fd49e9557293e
Commander.John.Crichton <none@none>
2011-02-15 19:41:58 +0000

    added copyright / license header Fixed Trailers scraper wouldn't fetch the last videos on the last page
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%40395

--- YouTubeScraperCore.py
+++ YouTubeScraperCore.py
@@ -1,3 +1,21 @@
+'''
+   YouTube plugin for XBMC
+   Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import sys, urllib, urllib2, re
 from BeautifulSoup import BeautifulSoup, SoupStrainer
 import YouTubeCore
@@ -535,7 +553,11 @@ class YouTubeScraperCore:
 							print "Scraper pagination failed, requested more than 10 pages which should never happen."
 						return False
 				
+				if (next == "false" and len(result) > per_page):
+					next = "true"
+					
 				result = result[:per_page]
+				
 				result[len(result) - 1]["next"] = next
 				params["page"] = request_page
 				return (result, status)
