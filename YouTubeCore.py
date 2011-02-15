b8aa4f469b5b06269b0ff7820173348df1c8a147
Commander.John.Crichton <none@none>
2011-02-15 19:40:37 +0000

    updated copyright / license year
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%40393

--- YouTubeCore.py
+++ YouTubeCore.py
@@ -1,6 +1,6 @@
 '''
     YouTube plugin for XBMC
-    Copyright (C) 2010 Tobias Ussing Senior And Henrik Mosgaard Jensen The Third
+    Copyright (C) 2010-2011 Tobias Ussing And Henrik Mosgaard Jensen
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
