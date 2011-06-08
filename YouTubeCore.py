5323fb33d2d01281a0e11fd4bd7391bad23cddc5
develop@tobiasussing.dk <develop@tobiasussing.dk>
2011-06-08 19:44:30 +0000

    further fixes
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%40665

--- YouTubeCore.py
+++ YouTubeCore.py
@@ -419,7 +419,7 @@ class YouTubeCore(object):
 		url = "http://gdata.youtube.com/feeds/api/users/default/playlists/%s" % get("playlist")
 		return self._fetchPage({"link": url, "api": "true", "login": "true", "auth": "true", "method": "DELETE"})
 
-	def a2dd_to_playlist(self, params = {}):
+	def add_to_playlist(self, params = {}):
 		get = params.get
 		url = "http://gdata.youtube.com/feeds/api/playlists/%s" % get("playlist")
 		add_request = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:yt="http://gdata.youtube.com/schemas/2007"><id>%s</id></entry>' % get("videoid")
