bffd6adc8817bcdda9949cdba936bb57750858f1
Commander.John.Crichton <none@none>
2011-07-29 20:03:12 +0000

    indentation errors
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%40935

--- YouTubeLogin.py
+++ YouTubeLogin.py
@@ -41,9 +41,9 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 	urls['http_login_confirmation'] = "http://www.youtube.com/signin?action_handle_signin=true&nomobiletemp=1&hl=en_US&next=/index&hl=en_US&ltmpl=sso"
 	urls['gdata_login'] = "https://www.google.com/accounts/ClientLogin"
 
-        __cj__ = cookielib.LWPCookieJar()
-        __opener__ = urllib2.build_opener(urllib2.HTTPCookieProcessor(__cj__))
-        urllib2.install_opener(__opener__)
+	__cj__ = cookielib.LWPCookieJar()
+	__opener__ = urllib2.build_opener(urllib2.HTTPCookieProcessor(__cj__))
+	urllib2.install_opener(__opener__)
 	
 	def __init__(self):
 		timeout = self.__settings__.getSetting( "timeout" )
@@ -85,61 +85,61 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 		xbmc.executebuiltin( "Container.Refresh" )
 
 	def _apiLogin(self, error = 0):
-                if self.__dbg__:
-                        print self.__plugin__ + " _apiLogin - errors: " + str(error)
-                        
+		if self.__dbg__:
+			print self.__plugin__ + " _apiLogin - errors: " + str(error)
+		
 		uname = self.__settings__.getSetting( "username" )
-                passwd = self.__settings__.getSetting( "user_password" )
-                
-                self.__settings__.setSetting('auth', "")
-                self.__settings__.setSetting('nick', "")
-                
-                if ( uname == "" or passwd == "" ):
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin no username or password set "
+		passwd = self.__settings__.getSetting( "user_password" )
+		
+		self.__settings__.setSetting('auth', "")
+		self.__settings__.setSetting('nick', "")
+		
+		if ( uname == "" or passwd == "" ):
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin no username or password set "
 			return ( "", 0 )
 
 		url = "https://accounts.google.com/o/oauth2/auth?client_id=208795275779.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=http%3A%2F%2Fgdata.youtube.com&response_type=code"
-                ret = self._fetchPage({ "link": url, "no-language-cookie": "true" })
+		ret = self._fetchPage({ "link": url, "no-language-cookie": "true" })
 		#print self.__plugin__ + " _apiLogin : " + repr(ret)
 
 		newurl = re.compile('<form action="(.*?)" method="POST">').findall(ret["content"])
 		if len(newurl) == 0:
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin no form method : " + repr(ret)
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin no form method : " + repr(ret)
 			return ( "", 0)
 
 		state_wrapper = re.compile('<input type="hidden" id="state_wrapper" name="state_wrapper" value="(.*?)">').findall(ret["content"])
 		if len(state_wrapper) == 0:
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin no state_wrapper "
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin no state_wrapper "
 			return ( "", 0)
-
+		
 		submit_approve_access = re.compile('<input id="submit_approve_access" name="submit_approve_access" type="submit" tabindex="1" value="(.*?)" class="').findall(ret["content"])
 		if len(submit_approve_access) == 0:
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin no submit_approve_access "
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin no submit_approve_access "
 			return ( "", 0)
 
 		url_data = { "state_wrapper": state_wrapper[0],
 			     "submit_approve_access": submit_approve_access[0]}
 		ret = self._fetchPage({ "link": newurl[0], "url_data": url_data, "no-language-cookie": "true" })
 
-                code = re.compile('code=(.*)</title>').findall(ret['content'])
+		code = re.compile('code=(.*)</title>').findall(ret['content'])
 		if len(code) == 0:
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin no 2-factor confirmation code found"
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin no 2-factor confirmation code found"
 			return ( "", 0)
 		code = code[0]
 
-                url = "https://accounts.google.com/o/oauth2/token"
+		url = "https://accounts.google.com/o/oauth2/token"
 		url_data = { "client_id": "208795275779.apps.googleusercontent.com",
 			     "client_secret": "sZn1pllhAfyonULAWfoGKCfp",
 			     "code": code,
 			     "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
 			     "grant_type": "authorization_code"}
-                ret = self._fetchPage({ "link": url, "url_data": url_data})
-
+		ret = self._fetchPage({ "link": url, "url_data": url_data})
+		
 		oauth = json.loads(ret["content"])
 		#print self.__plugin__ + " _apiLogin3 : " + repr(oauth)
 		self.__settings__.setSetting("oauth2_access_token", oauth["access_token"])
@@ -148,20 +148,20 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 		# use token
 
 		# curl https://www.google.com/m8/feeds/contacts/default/full?oauth_token=1/fFAGRNJru1FTz70BzhT3Zg
-                                        
-                if len(oauth) > 0:
+		
+		if len(oauth) > 0:
 			#self.__settings__.setSetting("oauth2_expires at", oauth["expires_in"] + current time. ) 
 			self.__settings__.setSetting("oauth2_access_token", oauth["access_token"])
 			self.__settings__.setSetting('auth', oauth["access_token"])
 			self.__settings__.setSetting("oauth2_refresh_token", oauth["refresh_token"])
 
-                        if self.__dbg__:
-                                print self.__plugin__ + " _apiLogin done: " + uname
-                        return ( self.__language__(30030), 200 )
-                                        
+			if self.__dbg__:
+				print self.__plugin__ + " _apiLogin done: " + uname
+			return ( self.__language__(30030), 200 )
+		
 		if self.__dbg__:
 			print self.__plugin__ + " _apiLogin default return. failing. " 
-                return ( self.__language__(30609), 303 )
+		return ( self.__language__(30609), 303 )
 	
 	def _httpLogin(self, new = False, error = 0):
 		if self.__dbg__:
@@ -230,7 +230,7 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 
 		if self.__dbg__:
 			print self.__plugin__ + " _httpLogin step 3 : " + repr(url_data)
-                ret = self._fetchPage({ "link": newurl[0], "no-language-cookie": "true", "url_data": url_data });
+		ret = self._fetchPage({ "link": newurl[0], "no-language-cookie": "true", "url_data": url_data });
 
 		# Login to youtube
 		newurl = re.compile('<meta http-equiv="refresh" content="0; url=&#39;(.*)&#39;"></head>').findall(ret["content"])
@@ -240,7 +240,7 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 				print self.__plugin__ + " _httpLogin: Normal login"
 			newurl = newurl[0].replace("&amp;", "&")
 
-		        # We need to do this twice now.
+			# We need to do this twice now.
 
 			if self.__dbg__:
 				print self.__plugin__ + " _httpLogin step 4 - normal login"
@@ -279,14 +279,17 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 			if self.__dbg__:
 				print self.__plugin__ + " _httpLogin step 4 - 2-factor login"
 			ret = self._fetchPage({ "link": "https://www.google.com/accounts/SmsAuth?persistent=yes", "url_data": url_data, "no-language-cookie": "true" })
-                        error = self.parseDOM(ret['content'], { "name": "div", "class": "error smaller", "content": "true"})
-                        if len(error) > 0:
+			
+			error = self.parseDOM(ret['content'], { "name": "div", "class": "error smaller", "content": "true"})
+			if len(error) > 0:
 				error = error[0]
 				error = urllib.unquote(error[0:error.find("<")]).replace("&#39;", "'")
-                                return ( error.strip(), 303)
+				return ( error.strip(), 303)
+			
 			smsToken = re.compile('<input type="hidden" name="smsToken" value="(.*?)">').findall(ret["content"])
 			if len(smsToken) == 0:
 				return ( "", 0)
+			
 			cont = re.compile('<input type="hidden" name="continue" value="(.*?)">').findall(ret["content"])
 			if len(cont) == 0:
 				return ( "", 0)
@@ -304,33 +307,35 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils):
 			newurl = re.compile('<meta http-equiv="refresh" content="0; url=&#39;(.*)&#39;"></head>').findall(ret["content"])
 			if len(newurl) == 0:
 				return ("", 0)
-                        newurl = newurl[0].replace("&amp;", "&").replace("http%25253A%252F", "http%253A%252F") # Google has an extra 25 in their code.
+			
+			newurl = newurl[0].replace("&amp;", "&").replace("http%25253A%252F", "http%253A%252F") # Google has an extra 25 in their code.
 
-                        if self.__dbg__:
-                                print self.__plugin__ + " new_url3: " + newurl
+			if self.__dbg__:
+				print self.__plugin__ + " new_url3: " + newurl
 
 			if self.__dbg__:
 				print self.__plugin__ + " _httpLogin step 6 - 2-factor login"
-                        ret = self._fetchPage({ "link": newurl});
+			ret = self._fetchPage({ "link": newurl});
 
-                        if self.__dbg__:
-                                print self.__plugin__ + " new_url4: " + repr(ret)
+			if self.__dbg__:
+				print self.__plugin__ + " new_url4: " + repr(ret)
+			
 			newurl = re.compile('<meta http-equiv="refresh" content="0; url=&#39;(.*)&#39;"></head>').findall(ret["content"])
 			if len(newurl) == 0:
 				return ("", 0)
-                        newurl = urllib.unquote(newurl[0])
+			newurl = urllib.unquote(newurl[0])
 
 			if self.__dbg__:
 				print self.__plugin__ + " _httpLogin step 7 - 2-factor login"
-                        ret = self._fetchPage({ "link": newurl, "no-language-cookie": "true"});
+			ret = self._fetchPage({ "link": newurl, "no-language-cookie": "true"});
 		else:
 			error = self.parseDOM(ret['content'], { "name": "div", "class": "errormsg", "content": "true"})
 			if len(error) > 0:
 				error = error[0]
 				error = urllib.unquote(error[0:error.find("[")]).replace("&#39;", "'")
 				return ( error.strip(), 303)
-                        if self.__dbg__:
-                                print self.__plugin__ + " _login couldn't find method to authenticate: " + repr(ret)
+				if self.__dbg__:
+					print self.__plugin__ + " _login couldn't find method to authenticate: " + repr(ret)
 
 			return ( "", 0)
 
