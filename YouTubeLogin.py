1e4f42e5e8f32632ef571a878e400481d08b31e3
Commander.John.Crichton <none@none>
2011-08-13 19:39:47 +0000

    cosmetics
    --HG--
    extra : convert_revision : svn%3A4b978fac-3306-7f52-0c4a-2d9971e34c12/trunk%401161

--- YouTubeLogin.py
+++ YouTubeLogin.py
@@ -16,7 +16,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-import sys, urllib, urllib2, socket, cookielib, time
+import sys, urllib2, cookielib, time
 try: import simplejson as json
 except ImportError: import json
 
@@ -45,7 +45,7 @@ class YouTubeLogin(YouTubeCore.YouTubeCore, YouTubeUtils.YouTubeUtils, CommonFun
 	__cj__ = cookielib.LWPCookieJar()
 	__opener__ = urllib2.build_opener(urllib2.HTTPCookieProcessor(__cj__))
 	urllib2.install_opener(__opener__)
-        __table_name__ = "YouTube"
+	__table_name__ = "YouTube"
 	
 	def login(self, params = {}):
 		if self.__dbg__:
