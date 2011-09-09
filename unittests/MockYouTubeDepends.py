c20093a38fcd1b6284c3c27b266d52440b477f4f
HenrikDK <none@none>
2011-09-10 00:38:19 +0200

    More work on Player Unit Tests
    --HG--
    branch : testing-framework

--- unittests/MockYouTubeDepends.py
+++ unittests/MockYouTubeDepends.py
@@ -1,22 +1,27 @@
-import sys
+import sys, string
 from mock import Mock, patch
 
 sys.path.append('../plugin/')
 
-import YoutubeUtils
-
+# Shield us from XBMC
 sys.modules["xbmc"] = __import__("mock")
 sys.modules["xbmcgui"] = __import__("mock")
 sys.modules["xbmcvfs"] = __import__("mock")
 sys.modules["xbmcplugin"] = __import__("mock")
 
+# Shield us from the file system
+sys.modules["__builtin__"].open = Mock() 
+
+#Emulate more of XBMC
 sys.modules[ "__main__" ].settings = Mock()
 sys.modules[ "__main__" ].settings.getAddonInfo = Mock()
 sys.modules[ "__main__" ].settings.getAddonInfo.return_value = "somepath"
 
 sys.modules[ "__main__" ].language = Mock()
 sys.modules[ "__main__" ].common = Mock()
+import YouTubeUtils
 sys.modules[ "__main__" ].utils = Mock(YouTubeUtils.YouTubeUtils)
+sys.modules[ "__main__" ].utils.VALID_CHARS = "-_.() %s%s" % (string.ascii_letters, string.digits)
 sys.modules[ "__main__" ].cache = Mock()
 sys.modules[ "__main__" ].core = Mock()
 sys.modules[ "__main__" ].feeds = Mock()
