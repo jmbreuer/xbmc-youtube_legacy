3a93e8e95aca69bf45b45b82b2325938d2f88537
HenrikDK <none@none>
2011-09-12 02:10:39 +0200

    removed a lot of stuff and rebroke unit tests
    --HG--
    branch : testing-framework

--- unittests/UnitTestSuite.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import YouTubePlayerTests
-
-if __name__ == "__main__":
-	suite = unittest.TestLoader().loadTestsFromTestCase(YouTubePlayerTests.YouTubePlayerTests)
-	unittest.TextTestRunner(verbosity=2).run(suite)
