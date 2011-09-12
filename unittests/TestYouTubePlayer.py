65224f175c04de030ef0d4f0e9549236a3956c7e
HenrikDK <none@none>
2011-09-12 23:53:41 +0200

    down to 2 failing unit tests cause i can't find real world examples for them
    --HG--
    branch : testing-framework

--- unittests/TestYouTubePlayer.py
+++ unittests/TestYouTubePlayer.py
@@ -7,11 +7,12 @@ from YouTubePlayer import YouTubePlayer
 class TestYouTubePlayer():
 		
 	def test_saveSubtitle_should_call_xbmcvfs_translatePath(self):
-		assert(1)
 		sys.modules["xbmcvfs"].translatePath = Mock()
 		sys.modules["xbmcvfs"].translatePath.return_value = "tempFilePath" 
 		player = YouTubePlayer()
+		
 		player.saveSubtitle("my_subtitle_stream",{"Title":"testTitle","videoid":"someVideoId","downloadPath":"downloadFilePath"})
+		
 		sys.modules["xbmcvfs"].translatePath.assert_called_with("special://temp")
 		
 	
@@ -19,29 +20,36 @@ class TestYouTubePlayer():
 		sys.modules["xbmcvfs"].translatePath = Mock()
 		sys.modules["xbmcvfs"].translatePath.return_value = "tempFilePath" 
 		player = YouTubePlayer()
-		player.saveSubtitle("my_subtitle_stream",{"Title":"testTitle","videoid":"someVideoId","downloadPath":"downloadFilePath"})
-		sys.modules["xbmcvfs"].rename.assert_called_with("tempFilePath/testTitle-[someVideoId].ssa","downloadFilePath/testTitle-[someVideoId].ssa")
 		
+		player.saveSubtitle("my_subtitle_stream",{"Title":"testTitle","videoid":"someVideoId","downloadPath":"downloadFilePath"})
+		
+		sys.modules["xbmcvfs"].rename.assert_called_with("tempFilePath/testTitle-[someVideoId].ssa","downloadFilePath/testTitle-[someVideoId].ssa")
+	
+	def test_getVideoUrlMap_should_return_empty_dictionary_on_missing_map(self):
+		player = YouTubePlayer()
+		
+		result = player.getVideoUrlMap({"args":{}},{})
+		
+		assert(result == {})
+	
 	def test_getVideoUrlMap_should_parse_streamMap(self):
 		player = YouTubePlayer()
+		
 		result = player.getVideoUrlMap(self.readTestInput("streamMapTest.txt"),{})
-		print "result length: " + repr(len(result)) + " result content: " + repr(result)
+		
 		assert(len(result) == 11)
-		assert(result.has_key(5))
-		assert(result.has_key(18))
-		assert(result.has_key(22))
-		assert(result.has_key(34))
-		assert(result.has_key(35))
-		assert(result.has_key(37))
-		assert(result.has_key(43))
-		assert(result.has_key(44))
-		assert(result.has_key(45))
-		assert(result.has_key(82))
-		assert(result.has_key(84))
-	
+		keys = [5, 18, 22, 34, 35, 37, 43, 44, 45, 82, 84]
+		for key in keys:
+			assert(key in result)
+		
 	def test_getVideoUrlMap_should_parse_url_encoded_stream_map(self):
 		player = YouTubePlayer()
 		result = player.getVideoUrlMap(self.readTestInput("urlEncodedStreamMapTest.txt"),{})
+		assert(len(result) == 11)
+		keys = [5, 18, 22, 34, 35, 37, 43, 44, 45, 82, 84]
+		for key in keys:
+			assert(key in result)
+
 		
 	def test_getVideoUrlMap_should_parse_url_map(self):
 		player = YouTubePlayer()
@@ -50,8 +58,12 @@ class TestYouTubePlayer():
 	def test_getVideoUrlMap_should_mark_live_play(self):
 		player = YouTubePlayer()
 		video = {}
+		
 		result = player.getVideoUrlMap(self.readTestInput("liveStreamTest.txt"),video)
-		assert(video["live_play"] == "true") 
+		
+		assert(video["live_play"] == "true")
+		assert(len(result) == 1)
+		assert(result.has_key(34)) 
 	
 	def test_playVideo_should_call_getObject(self):
 		player = YouTubePlayer()
