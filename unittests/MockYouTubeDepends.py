e346a10073fd9ea3afa2cd047d95d7e100977fe4
HenrikDK <none@none>
2011-09-14 11:21:19 +0200

    changed main mock to be a function so we can run it multiple times
    --HG--
    branch : testing-framework

--- unittests/MockYouTubeDepends.py
+++ unittests/MockYouTubeDepends.py
@@ -1,36 +1,40 @@
-import sys, string
-import sys, inspect
-from mock import Mock, patch
+class MockYouTubeDepends:
+	common = ""
+	def mock(self):
+		import sys, string 
+		from mock import Mock
+				
+		#Emulate more of XBMC
+		sys.modules[ "__main__" ].plugin = "unittest"
+		sys.modules[ "__main__" ].dbg = True
+		sys.modules[ "__main__" ].dbglevel = 1
+		sys.modules[ "__main__" ].settings = Mock()
+		sys.modules[ "__main__" ].settings.getAddonInfo = Mock()
+		sys.modules[ "__main__" ].settings.getAddonInfo.return_value = "somepath"
+		sys.modules[ "__main__" ].language = Mock()
+		
+		import YouTubeLogin 
+		sys.modules[ "__main__" ].login = Mock(spec=YouTubeLogin.YouTubeLogin)
+		
+		import YouTubeStorage
+		sys.modules[ "__main__" ].storage = Mock(spec=YouTubeStorage.YouTubeStorage)
 
-# Shield us from XBMC
-sys.modules["xbmc"] = __import__("mock")
-sys.modules["xbmcgui"] = __import__("mock")
-sys.modules["xbmcvfs"] = __import__("mock")
-sys.modules["xbmcvfs"].rename = Mock()
-sys.modules["xbmcplugin"] = __import__("mock")
+		import CommonFunctions
+		sys.modules[ "__main__" ].common = Mock(spec = CommonFunctions.CommonFunctions)
+		sys.modules[ "__main__" ].common.log = Mock() 
+		
+		import YouTubeUtils
+		sys.modules[ "__main__" ].utils = Mock(spec=YouTubeUtils.YouTubeUtils)
+		sys.modules[ "__main__" ].utils.VALID_CHARS = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
-# Shield us from the file system
-sys.modules["__builtin__"].open = Mock() 
-
-#Emulate more of XBMC
-sys.modules[ "__main__" ].settings = Mock()
-sys.modules[ "__main__" ].settings.getAddonInfo = Mock()
-sys.modules[ "__main__" ].settings.getAddonInfo.return_value = "somepath"
-
-sys.modules[ "__main__" ].language = Mock()
-sys.modules[ "__main__" ].common = Mock()
-import YouTubeUtils
-sys.modules[ "__main__" ].utils = Mock(YouTubeUtils.YouTubeUtils)
-sys.modules[ "__main__" ].utils.VALID_CHARS = "-_.() %s%s" % (string.ascii_letters, string.digits)
-sys.modules[ "__main__" ].cache = Mock()
-sys.modules[ "__main__" ].core = Mock()
-sys.modules[ "__main__" ].feeds = Mock()
-sys.modules[ "__main__" ].scraper = Mock()
-sys.modules[ "__main__" ].login = Mock()
-sys.modules[ "__main__" ].plugin = "unittest"
-sys.modules[ "__main__" ].dbg = False
-sys.modules[ "__main__" ].dbglevel = 1
-sys.modules[ "__main__" ].storage = Mock()
-
-sys.modules["xbmc"].translatePath = Mock()
-sys.modules["xbmc"].translatePath.return_value = "testing"
+		sys.modules[ "__main__" ].cache = Mock()
+		
+		import YouTubeCore
+		sys.modules[ "__main__" ].core = Mock(spec=YouTubeCore.YouTubeCore)
+		import YouTubeFeeds
+		sys.modules[ "__main__" ].feeds = Mock(spec=YouTubeFeeds.YouTubeFeeds)
+		import YouTubeScraper 
+		sys.modules[ "__main__" ].scraper = Mock(spec=YouTubeScraper.YouTubeScraper)
+		import YouTubePlayer
+		sys.modules[ "__main__" ].player = Mock(spec=YouTubePlayer.YouTubePlayer)
+		
