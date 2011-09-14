f8247726c6c54bf6c2ef6b9a56b5cb3a140d52b0
HenrikDK <none@none>
2011-09-14 11:20:18 +0200

    moved mocking of xbmc depends to base class and reworked how default mocking works by using setup function instead
    --HG--
    branch : testing-framework

--- unittests/BaseTestCase.py
+++ unittests/BaseTestCase.py
@@ -1,8 +1,22 @@
 import sys
 import unittest2, io
+from mock import Mock
+import MockYouTubeDepends
+# Shield us from XBMC
+sys.modules["xbmc"] = __import__("mock")
+sys.modules["xbmcgui"] = __import__("mock")
+sys.modules["xbmcvfs"] = __import__("mock")
+sys.modules["xbmcvfs"].rename = Mock()
+sys.modules["xbmcplugin"] = __import__("mock")
+sys.modules["xbmc"].translatePath = Mock()
+sys.modules["xbmc"].translatePath.return_value = "testing"
+
 sys.path.append('../plugin/')
 
 class BaseTestCase(unittest2.TestCase):
+	
+	def setUp(self):
+		MockYouTubeDepends.MockYouTubeDepends().mock()
 		
 	def readTestInput(self, filename, should_eval = True):
 		testinput = io.open("resources/" + filename)
