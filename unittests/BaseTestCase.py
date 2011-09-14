e595f3a14cf29b7cf05968f2fe8dbef880f8f798
HenrikDK <none@none>
2011-09-14 15:58:17 +0200

    expanded mocking of xbmc in test case
    --HG--
    branch : testing-framework

--- unittests/BaseTestCase.py
+++ unittests/BaseTestCase.py
@@ -4,7 +4,11 @@ from mock import Mock
 import MockYouTubeDepends
 # Shield us from XBMC
 sys.modules["xbmc"] = __import__("mock")
+sys.modules["xbmc"].getSkinDir = Mock()
+sys.modules["xbmc"].getSkinDir.return_value = "testSkinPath"
 sys.modules["xbmcgui"] = __import__("mock")
+sys.modules["xbmcgui"].WindowXMLDialog = Mock()
+sys.modules["xbmcgui"].WindowXMLDialog.return_value = "testWindowXML"
 sys.modules["xbmcvfs"] = __import__("mock")
 sys.modules["xbmcvfs"].rename = Mock()
 sys.modules["xbmcplugin"] = __import__("mock")
@@ -17,7 +21,7 @@ class BaseTestCase(unittest2.TestCase):
 	
 	def setUp(self):
 		MockYouTubeDepends.MockYouTubeDepends().mock()
-		
+	
 	def readTestInput(self, filename, should_eval = True):
 		testinput = io.open("resources/" + filename)
 		inputdata = testinput.read()
