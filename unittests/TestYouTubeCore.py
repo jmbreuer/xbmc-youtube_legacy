110be6dcc50526a9aa71cbba33d37dbeab1ba48d
HenrikDK <none@none>
2011-09-28 00:18:44 +0200

    added more unit tests and added more scenarios
    --HG--
    branch : testing-framework

--- unittests/TestYouTubeCore.py
+++ unittests/TestYouTubeCore.py
@@ -62,7 +62,7 @@ class TestYouTubeCore(BaseTestCase.BaseTestCase):
 		assert(core._fetchPage.call_count == 1)
 		core._fetchPage.assert_called_with({"request":request,"link": delete_url, "api": "true", "login": "true", "auth": "true"})
 
-	def test_add_favorite_shouldcall_fetchPage_with_correct_fetch_options(self):
+	def test_add_favorite_should_call_fetchPage_with_correct_fetch_options(self):
 		settings = ["4","3" ]
 		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
@@ -70,52 +70,178 @@ class TestYouTubeCore(BaseTestCase.BaseTestCase):
 		core._fetchPage.return_value = {"content":"success", "status":200}
 		url = "http://gdata.youtube.com/feeds/api/users/default/favorites"		
 		request = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom"><id>some_id</id></entry>'
+		
 		core.add_favorite({ "editid": "edit_id", "videoid":"some_id" })
 		
 		assert(core._fetchPage.called)
 		assert(core._fetchPage.call_count == 1)
 		core._fetchPage.assert_called_with({"request":request,"link": url, "api": "true", "login": "true", "auth": "true"})
 
-	def add_subscription(self):
+	def test_add_subscription_should_call_fetchPage_with_correct_fetch_options(self):
+		settings = ["4","3" ]
+		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
-		result = core.add_subscription({})
-		assert(result == [])
-
-	def add_playlist(self):
+		core._fetchPage = Mock()
+		core._fetchPage.return_value = {"content":"success", "status":200}
+		url = "http://gdata.youtube.com/feeds/api/users/default/subscriptions"		
+		request = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:yt="http://gdata.youtube.com/schemas/2007"> <category scheme="http://gdata.youtube.com/schemas/2007/subscriptiontypes.cat" term="user"/><yt:username>channel</yt:username></entry>'
+		
+		core.add_subscription({ "channel": "channel"})
+		
+		assert(core._fetchPage.called)
+		assert(core._fetchPage.call_count == 1)
+		core._fetchPage.assert_called_with({"request":request,"link": url, "api": "true", "login": "true", "auth": "true"})
+		
+	def test_add_playlist_should_call_fetchPage_with_correct_fetch_options(self):
+		settings = ["4","3" ]
+		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
-		result = core.add_playlist({})
-		assert(result == [])
+		core._fetchPage = Mock()
+		core._fetchPage.return_value = {"content":"success", "status":200}
+		url = "http://gdata.youtube.com/feeds/api/users/default/playlists"		
+		request = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:yt="http://gdata.youtube.com/schemas/2007"><title type="text">some_title</title><summary>some_summary</summary></entry>'
+		
+		core.add_playlist({ "title": "some_title", "summary":"some_summary"})
+		
+		assert(core._fetchPage.called)
+		assert(core._fetchPage.call_count == 1)
+		core._fetchPage.assert_called_with({"request":request,"link": url, "api": "true", "login": "true", "auth": "true"})
 
-	def del_playlist(self):
+	def test_del_playlist_should_call_fetchPage_with_correct_fetch_options(self):
+		settings = ["4","3" ]
+		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
-		result = core.del_playlist({})
-		assert(result == [])
+		core._fetchPage = Mock()
+		core._fetchPage.return_value = {"content":"success", "status":200}
+		url = "http://gdata.youtube.com/feeds/api/users/default/playlists/some_playlist"
+		
+		core.del_playlist({ "playlist": "some_playlist"})
+		
+		assert(core._fetchPage.called)
+		assert(core._fetchPage.call_count == 1)
+		core._fetchPage.assert_called_with({"link": url, "api": "true", "login": "true", "auth": "true","method":"DELETE"})
 
-	def add_to_playlist(self):
+	def test_add_to_playlist_should_call_fetchPage_with_correct_fetch_options(self):
+		settings = ["4","3" ]
+		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
-		result = core.add_to_playlist({})
-		assert(result == [])
+		core._fetchPage = Mock()
+		core._fetchPage.return_value = {"content":"success", "status":200}
+		url = "http://gdata.youtube.com/feeds/api/playlists/some_playlist"
+		request = '<?xml version="1.0" encoding="UTF-8"?><entry xmlns="http://www.w3.org/2005/Atom" xmlns:yt="http://gdata.youtube.com/schemas/2007"><id>some_id</id></entry>'
+		
+		core.add_to_playlist({ "playlist": "some_playlist","videoid":"some_id"})
+		
+		assert(core._fetchPage.called)
+		assert(core._fetchPage.call_count == 1)
+		core._fetchPage.assert_called_with({"request":request,"link": url, "api": "true", "login": "true", "auth": "true"})
 
-	def remove_from_playlist(self):
+	def test_remove_from_playlist_should_call_fetchPage_with_correct_fetch_options(self):
+		settings = ["4","3" ]
+		sys.modules[ "__main__" ].settings.getSetting.side_effect = lambda x: settings.pop()
 		core = YouTubeCore()
-		result = core.remove_from_playlist({})
-		assert(result == [])
+		core._fetchPage = Mock()
+		core._fetchPage.return_value = {"content":"success", "status":200}
+		url = "http://gdata.youtube.com/feeds/api/playlists/some_playlist/some_entry_id"
+		
+		core.remove_from_playlist({ "playlist": "some_playlist","playlist_entry_id":"some_entry_id"})
+		
+		assert(core._fetchPage.called)
+		assert(core._fetchPage.call_count == 1)
+		core._fetchPage.assert_called_with({"link": url, "api": "true", "login": "true", "auth": "true","method":"DELETE"})
 
-	def getFolderInfo(self):
+	def ttest_getFolderInfo_should_use_getElementsByTagName_to_look_for_link_and_entries(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+		assert(False)
+
+	def ttest_getFolderInfo_should_search_links_relation_attribute_for_multiple_pages(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+		
+	def ttest_getFolderInfo_should_find_edit_id_in_xml_structure_if_id_tag_is_present(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+
+	def ttest_getFolderInfo_should_find_edit_id_in_xml_structure_if_link_tag_is_present(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+
+	def ttest_getFolderInfo_should_set_item_params_correctly_for_contacts_feed(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+
+	def ttest_getFolderInfo_should_set_item_params_correctly_for_subscriptions_feed(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+
+	def ttest_getFolderInfo_should_set_item_params_correctly_for_playlist_feed(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+
+	def ttest_getFolderInfo_should_call_storage_retrieve_to_find_thumbnail(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+	
+	def ttest_getFolderInfo_should_call_utils_addNextFolder_to_set_default_next_folder_on_feed(self):
+		core = YouTubeCore()
+		xml = ""
+		core._getFolderInfo(xml, {})
+
+		assert(False)
+	
+	def ttest_getBatchDetailsOverride_should_call_getBatchDetails_with_list_of_video_ids(self):
 		core = YouTubeCore()
 		result = core.getBatchDetailsOverride(items, params={})
 		assert(result == [])
 
-	def getBatchDetailsOverride(self):
+	def ttest_getBatchDetailsOverride_should_override_specified_properties_in_output_from_getBatchDetails(self):
 		core = YouTubeCore()
 		result = core.getBatchDetailsOverride(items, params={})
 		assert(result == [])
 
-	def getBatchDetailsThumbnails(self):
+	def ttest_getBatchDetailsThumbnails_should_call_getBatchDetails_with_list_of_video_ids(self):(self):
 		core = YouTubeCore()
 		result = core.getBatchDetailsThumbnails(items, params={})
 		assert(result == [])
 
+	def ttest_getBatchDetailsThumbnails_should_override_thumbnails_in_output_from_getBatchDetails(self):
+		core = YouTubeCore()
+		result = core.getBatchDetailsThumbnails(items, params={})
+		assert(result == [])
+		
+	def ttest_getBatchDetailsThumbnails_should_fill_out_missing_videos_in_collection_from_getBatchDetails_to_maintain_collection_size(self):
+		core = YouTubeCore()
+		result = core.getBatchDetailsThumbnails(items, params={})
+		assert(result == [])
+
+	def ttest_getBatchDetailsOverride_should_fill_out_missing_videos_in_collection_from_getBatchDetails_to_maintain_collection_size(self):
+		core = YouTubeCore()
+		result = core.getBatchDetailsOverride(items, params={})
+		assert(result == [])
+		
 	def getBatchDetails(self):
 		core = YouTubeCore()
 		result = core._fetchPage(params={})
