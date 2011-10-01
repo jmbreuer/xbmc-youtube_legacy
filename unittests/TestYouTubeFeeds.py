90d172ad47bd339d789051d32f8ea76ce0bdc413
HenrikDK <none@none>
2011-10-02 01:50:48 +0200

    more unittesting
    --HG--
    branch : testing-framework

--- unittests/TestYouTubeFeeds.py
+++ unittests/TestYouTubeFeeds.py
@@ -20,13 +20,38 @@ class TestYouTubeFeeds(BaseTestCase.BaseTestCase):
 		feeds.createUrl()
 		
 		sys.modules[ "__main__" ].settings.getSetting.assert_called_with("region_id")
+
+	def test_createUrl_should_get_correct_feed_url_if_feed_is_in_params(self):
+		sys.modules[ "__main__" ].settings.getSetting.return_value = "1"
+		feeds = YouTubeFeeds()
+		
+		result = feeds.createUrl({"feed":"favorites"})
+		result = result[:result.find("?")]
+		url = feeds.urls["favorites"] % ("default")
+		sys.modules[ "__main__" ].settings.getSetting.assert_called_with("region_id")
+		assert( result == url)
+
+	def test_createUrl_should_get_correct_user_feed_url_if_user_feed_is_in_params(self):
+		sys.modules[ "__main__" ].settings.getSetting.return_value = "1"
+		feeds = YouTubeFeeds()
+		
+		result = feeds.createUrl({"user_feed":"favorites"})
+		result = result[:result.find("?")]
+		url = feeds.urls["favorites"] % ("default")
+		sys.modules[ "__main__" ].settings.getSetting.assert_called_with("region_id")
+		assert( result == url)
+		
+	def test_createUrl_should_get_correct_search_url_if_search_is_in_params(self):
+		sys.modules[ "__main__" ].settings.getSetting.return_value = "1"
+		feeds = YouTubeFeeds()
+		
+		result = feeds.createUrl({"search":"some_search"})
+		result = result[:result.find("moderate") + 8]
+		url = feeds.urls["search"] % ("some_search","moderate")
+		sys.modules[ "__main__" ].settings.getSetting.assert_called_with("region_id")
+		assert( result == url)
+
 '''
-	def ttest_createUrl_should_2
-		assert(False)
-
-	def ttest_createUrl_should_3
-		assert(False)
-
 	def ttest_createUrl_should_4
 		assert(False)
 
