cc7e4df5acf317d4d7d970b3f0421af4d467064c
HenrikDK <none@none>
2011-10-02 21:14:53 +0200

    adde more tests
    --HG--
    branch : testing-framework

--- unittests/TestYouTubeFeeds.py
+++ unittests/TestYouTubeFeeds.py
@@ -115,5 +115,91 @@ class TestYouTubeFeeds(BaseTestCase.BaseTestCase):
 		
 		assert(result.find("?time=this_week") > 0)
 	
+	def test_list_should_call_listFolder_if_folder_is_in_params(self):
+		assert(False)
+		
+	def test_list_should_call_listPlaylist_if_playlist_is_in_params(self):
+		assert(False)
+		
+	def test_list_should_call_core_getAuth_to_test_if_login_is_set_if_login_is_in_params(self):
+		assert(False)
+		
+	def test_list_should_return_error_message_if_login_is_not_set_and_login_is_in_params(self):
+		assert(False)
+		
+	def test_list_should_call_createUrl_to_fetch_url(self):
+		assert(False)
+		
+	def test_list_should_call_core_fetchPage_to_get_feed_content(self):
+		assert(False)
+		
+	def test_list_should_call_return_content_and_status_if_fetchPage_failed(self):
+		assert(False)
+		
+	def test_list_should_call_core_getVideoInfo_if_fetchPage_succeded_and_folder_is_not_in_params(self):
+		assert(False)
+		
+	def test_list_should_return_error_status_if_video_list_is_empty(self):
+		assert(False)
+		
+	def test_list_should_call_storage_store_to_save_first_thumbnail_in_list(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_getSetting_to_get_perpage(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_storage_retrieve_to_fetch_cached_video_listing(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_getBatchDetailsOverride_to_fetch_video_info_for_stored_video_list(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_listAll_if_stored_list_isnt_found(self):
+		assert(False)
+		
+	def test_listPlaylist_should_return_error_status_if_listAll_returns_empty_list(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_storage_store_with_list_of_video_ids_and_entryids(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_storage_store_with_first_thumbnail_of_list(self):
+		assert(False)
+		
+	def test_listPlaylist_should_call_addNextFolder_for_lists_longer_than_perpage(self):
+		assert(False)
+		
+	def test_listPlaylist_should_limit_list_lengt_to_perpage_count(self):
+		assert(False)
+		
+	def test_listPlaylist_should_starts_list_position_from_page_count_and_perpage_count(self):
+		assert(False)
+		
+	def test_listFolder_should_call_storage_getUserOptionFolder_if_storage_is_contact_options(self):
+		assert(False)
+	
+	def test_listFolder_should_call_storage_getStoredSearches_if_storage_is_set_but_not_contact_options(self):
+		assert(False)
+
+	def test_listFolder_should_call_getSetting_to_get_perpage(self):
+		assert(False)
+		
+	def test_listFolder_should_call_storage_retrieve_to_fetch_cached_video_listing(self):
+		assert(False)
+
+	def test_listFolder_should_call_listAll_if_stored_list_isnt_found(self):
+		assert(False)
+
+	def test_listFolder_should_call_storage_store_to_save_new_list(self):
+		assert(False)
+
+	def test_listFolder_should_call_addNextFolder_for_lists_longer_than_perpage(self):
+		assert(False)
+		
+	def test_listFolder_should_limit_list_lengt_to_perpage_count(self):
+		assert(False)
+		
+	def test_listFolder_should_starts_list_position_from_page_count_and_perpage_count(self):
+		assert(False)	
 if __name__ == "__main__":
 	nose.runmodule()
