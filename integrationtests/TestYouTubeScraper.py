cb45da7f2205ff708c24d809cd0c0fa5784834f8
HenrikDK <none@none>
2011-10-07 20:29:26 +0200

    added initial integration test of youtube scraper
    --HG--
    branch : testing-framework

---
+++ integrationtests/TestYouTubeScraper.py
@@ -0,0 +1,14 @@
+import MockYouTubeDepends
+import BaseTestCase
+import unittest2
+import nose
+from mock import Mock, patch
+from YouTubeScraper import YouTubeScraper
+
+class YouTubePlayerTests(BaseTestCase.BaseTestCase):
+	
+	def ttest_scrapeTrailersListFormat_should_scraper_recent_trailers_correctly(self):
+		assert(False)
+		
+if __name__ == "__main__":
+	nose.runmodule()
