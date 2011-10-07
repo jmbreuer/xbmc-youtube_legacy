96a2dbe91eb26df90b4cc97b5665a39dcb5537df
HenrikDK <none@none>
2011-10-07 20:29:47 +0200

    added modified version of base test case from unit tests
    --HG--
    branch : testing-framework

---
+++ integrationtests/BaseTestCase.py
@@ -0,0 +1,14 @@
+import sys
+import unittest2, io
+from mock import Mock
+import MockYouTubeDepends
+
+MockYouTubeDepends.MockYouTubeDepends().mockXBMC()
+
+sys.path.append('../plugin/')
+sys.path.append('../xbmc-mocks/')
+
+class BaseTestCase(unittest2.TestCase):
+	
+	def setUp(self):
+		MockYouTubeDepends.MockYouTubeDepends().mockXBMC()
