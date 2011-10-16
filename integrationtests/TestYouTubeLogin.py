abce255b87816d6105d03e104bdd928c9f6d0a56
Tobias Ussing <develop@tobiasussing.dk>
2011-10-17 01:05:26 +0200

    Basic 2-factor
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeLogin.py
+++ integrationtests/TestYouTubeLogin.py
@@ -1,14 +1,30 @@
 import BaseTestCase
 import unittest2
-import nose, sys
+import nose, sys, os
 from mock import Mock, patch
 
 
 class TestYouTubeLogin(BaseTestCase.BaseTestCase):
-	
 	def test_login_should_perform_basic_login_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
 		
+		self.navigation.executeAction({"action":"settings"})
+		
+		print "nick: " + sys.modules["__main__"].settings.getSetting("nick")
+		print "username: " + sys.modules["__main__"].settings.getSetting("username")
+		print "pass: " + sys.modules["__main__"].settings.getSetting("user_password")
+		print "auth: " + sys.modules["__main__"].settings.getSetting("auth")
+		print "oauth2_access_token: " + sys.modules["__main__"].settings.getSetting("oauth2_access_token")
+		assert(False)
+
+	def test_login_should_perform_basic_2factor_login_correctly(self):
+		sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'pyotp-src'))
+		import pyotp
+		totp = pyotp.TOTP("fbfkkk27ffmaihzg")
+		userpin = totp.now()
+		print "OTP: " + str(userpin)
+		sys.modules["__main__"].settings.load_strings("./resources/2factor-login-settings.xml")
+		sys.modules["__main__"].xbmc.Keyboard().getText.side_effect = [str(userpin)]
 		self.navigation.executeAction({"action":"settings"})
 		
 		print "nick: " + sys.modules["__main__"].settings.getSetting("nick")
