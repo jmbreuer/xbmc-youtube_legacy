20433b657bbe630ade195789dddd827b2b1f694b
HenrikDK <none@none>
2011-10-16 11:19:08 +0200

    changed base test case to initialize plugin correctly
    --HG--
    branch : testing-framework

--- integrationtests/BaseTestCase.py
+++ integrationtests/BaseTestCase.py
@@ -11,4 +11,32 @@ sys.path.append('../xbmc-mocks/')
 class BaseTestCase(unittest2.TestCase):
 	
 	def setUp(self):
+		MockYouTubeDepends.MockYouTubeDepends().mock()
 		MockYouTubeDepends.MockYouTubeDepends().mockXBMC()
+		self.intializePlugin()
+		
+	def intializePlugin(self):
+		import YouTubeUtils
+		sys.modules[ "__main__" ].utils = YouTubeUtils.YouTubeUtils()
+		import CommonFunctions
+		sys.modules[ "__main__" ].common = CommonFunctions.CommonFunctions() 
+		import YouTubeStorage
+		sys.modules[ "__main__" ].storage = YouTubeStorage.YouTubeStorage()
+		import YouTubeCore
+		sys.modules[ "__main__" ].core = YouTubeCore.YouTubeCore()
+		import YouTubeLogin 
+		sys.modules[ "__main__" ].login = YouTubeLogin.YouTubeLogin()
+		import YouTubeFeeds
+		sys.modules[ "__main__" ].feeds = YouTubeFeeds.YouTubeFeeds()
+		import YouTubeScraper 
+		sys.modules[ "__main__" ].scraper = YouTubeScraper.YouTubeScraper()
+		import YouTubePlayer
+		sys.modules[ "__main__" ].player = YouTubePlayer.YouTubePlayer()
+		import YouTubeDownloader
+		sys.modules[ "__main__" ].downloader = YouTubeDownloader.YouTubeDownloader()
+		import YouTubeScraper
+		sys.modules[ "__main__" ].scraper = YouTubeScraper.YouTubeScraper()
+		import YouTubePlaylistControl
+		sys.modules[ "__main__" ].playlist = YouTubePlaylistControl.YouTubePlaylistControl()
+		import YouTubeNavigation
+		sys.modules[ "__main__" ].navigation = YouTubeNavigation.YouTubeNavigation()
