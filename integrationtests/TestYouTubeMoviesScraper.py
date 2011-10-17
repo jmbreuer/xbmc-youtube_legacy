4d639db3c6e91c12d0574da394ad263cd4884d29
HenrikDK <none@none>
2011-10-18 01:06:19 +0200

    more work on scraper tests
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeMoviesScraper.py
+++ integrationtests/TestYouTubeMoviesScraper.py
@@ -10,33 +10,35 @@ class TestYouTubeTrailersScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_folder_list()
+		self.assert_directory_items_should_have_thumbnails()
 		self.assert_directory_items_contain("category")
 		
-	def ttest_plugin_should_scrape_movie_sub_category_listing_correctly(self):
-		self.navigation.listMenu({"scraper":"movies", "folder":"true", "category":"indian_cinema", "path":"/root/explore/movies"})
+	def test_plugin_should_scrape_movie_sub_category_listing_correctly(self):
+		self.navigation.listMenu({"scraper":"movies", "folder":"true", "category":"indian_cinema", "subcategory":"true", "path":"/root/explore/movies"})
 		
 		self.assert_directory_count_greater_than_or_equals(1)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_folder_list()
 
-	def ttest_plugin_should_scrape_movie_category_video_list_correctly(self):
-		self.navigation.listMenu({"scraper":"movies", "folder":"true", "category":"comedy", "path":"/root/explore/movies"})
+	def test_plugin_should_scrape_movie_category_video_list_correctly(self):
+		self.navigation.listMenu({"scraper":"movies", "category":"comedy", "path":"/root/explore/movies"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_thumbnails()
+		self.assert_directory_items_should_have_external_thumbnails()
 		self.assert_directory_items_should_have_poster_thumbnails()
+		self.assert_directory_should_have_next_folder()
 	
-	def ttest_plugin_should_scrape_movie_sub_category_video_list_correctly(self):
-		self.navigation.listMenu({"scraper":"movies", "folder":"true", "category":"indian_cinema", "path":"/root/explore/movies"})
+	def test_plugin_should_scrape_movie_sub_category_video_list_correctly(self):
+		self.navigation.listMenu({"scraper":"movies", "category":"indian-cinema?fl=f&l=te&pt=g&st=f", "path":"/root/explore/movies"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_thumbnails()
+		self.assert_directory_items_should_have_external_thumbnails()
 		self.assert_directory_items_should_have_poster_thumbnails()
 		self.assert_directory_should_have_next_folder()
 
