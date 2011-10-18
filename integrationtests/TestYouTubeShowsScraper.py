2372b6f856952665a6dc98bd7e25a1849cc8b6e3
HenrikDK <none@none>
2011-10-18 21:25:12 +0200

    fixed shows scraper
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeShowsScraper.py
+++ integrationtests/TestYouTubeShowsScraper.py
@@ -3,7 +3,7 @@ import nose
 
 class TestYouTubeShowsScraper(BaseTestCase.BaseTestCase):
 	
-	def ttest_plugin_should_scrape_shows_category_listing_correctly(self):
+	def test_plugin_should_scrape_shows_category_listing_correctly(self):
 		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/shows", "folder":"true"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -12,7 +12,7 @@ class TestYouTubeShowsScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("category")
 		self.assert_directory_items_should_have_thumbnails()
 	
-	def ttest_plugin_should_scrape_shows_category_show_list_correctly(self):
+	def test_plugin_should_scrape_show_list_correctly(self):
 		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/shows","category":"comedy%3Ffeature%3Dsh_c%26amp%3Bpt%3Dg%26amp%3Bl%3Den"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -22,56 +22,39 @@ class TestYouTubeShowsScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_scrape_show_episode_video_list_correctly(self):
-		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/trailers/current", "show":"roosterteethshorts%3Ffeature%3Dsh_co_show_1_17"})
+		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/trailers/current", "show":"minecraft?feature=sh_gm_show_1_1", "season":"3"})
 		
 		self.assert_directory_count_greater_than_or_equals(30)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
-		self.assert_directory_items_should_have_poster_thumbnails()
 
-	def ttest_plugin_should_scrape_show_season_folder_list_correctly(self):
-		self.navigation.listMenu({"scraper":"upcoming_trailers","path":"/root/explore/trailers/upcoming"})
+	def test_plugin_should_scrape_show_season_folder_list_correctly(self):
+		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/trailers/current", "show":"minecraft?feature=sh_gm_show_1_1"})
 		
-		self.assert_directory_count_greater_than_or_equals(1)
+		self.assert_directory_count_greater_than_or_equals(5)
+		self.assert_directory_count_less_than_or_equals(51)
+		self.assert_directory_is_a_folder_list()
+		self.assert_directory_items_contain("season")
+
+	def test_plugin_should_scrape_show_season_episode_video_list_correctly(self):
+		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/trailers/current", "show":"minecraft?feature=sh_gm_show_1_1", "season":"3"})
+		
+		self.assert_directory_count_greater_than_or_equals(30)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_thumbnails()
-		self.assert_directory_items_should_have_poster_thumbnails()
+		self.assert_directory_items_should_have_external_thumbnails()
 
-	def ttest_plugin_should_scrape_show_season_episode_video_list_correctly(self):
-		self.navigation.listMenu({"scraper":"popular_trailers","path":"/root/explore/trailers/popular"})
+	def test_plugin_should_scrape_show_season_episode_video_list_page_2_correctly(self):
+		self.navigation.listMenu({"scraper":"shows","path":"/root/explore/trailers/current", "show":"minecraft?feature=sh_gm_show_1_1", "season":"3", "page":"1"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_thumbnails()
-		self.assert_directory_items_should_have_poster_thumbnails()
-		self.assert_directory_should_have_next_folder()
-
-	def ttest_plugin_should_scrape_show_season_episode_video_list_page_2_correctly(self):
-		self.navigation.listMenu({"scraper":"latest_game_trailers","path":"/root/explore/trailers/popular"})
 		
-		self.assert_directory_count_greater_than_or_equals(10)
-		self.assert_directory_count_less_than_or_equals(51)
-		self.assert_directory_is_a_video_list()
-		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_thumbnails()
-		self.assert_directory_items_should_have_poster_thumbnails()
-	
-	def ttest_plugin_should_scrape_show_episode_video_list_page_2_correctly(self):
-		self.navigation.listMenu({"scraper":"popular_game_trailers","path":"/root/explore/trailers/popular"})
-		
-		self.assert_directory_count_greater_than_or_equals(10)
-		self.assert_directory_count_less_than_or_equals(51)
-		self.assert_directory_is_a_video_list()
-		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_thumbnails()
-		self.assert_directory_items_should_have_poster_thumbnails()
-		self.assert_directory_should_have_next_folder()
-	
 if __name__ == "__main__":
 	nose.runmodule()
