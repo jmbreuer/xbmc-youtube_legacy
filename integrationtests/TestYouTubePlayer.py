e494b58116f7f50887dade497f5bcbae5ebd6a73
HenrikDK <none@none>
2011-10-20 00:12:05 +0200

    changed how player tests are called
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubePlayer.py
+++ integrationtests/TestYouTubePlayer.py
@@ -1,14 +1,13 @@
 import BaseTestCase
 import unittest2
 import nose, sys, os
-from mock import Mock, patch
-
 
 class TestYouTubePlayer(BaseTestCase.BaseTestCase):
+	
 	def test_playVideo_should_play_videos(self):
-                sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-		player = sys.modules[ "__main__" ].player
-		player.playVideo({ "videoid": "54VJWHL2K3I"})
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		
+		self.navigation.executeAction({"action":"play_video", "videoid": "54VJWHL2K3I"})
 
 		args = sys.modules[ "__main__" ].xbmcplugin.setResolvedUrl.call_args_list
 		print "Args: " + repr(args)
@@ -21,9 +20,9 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		assert(args[0][1]["succeeded"] == True)
 
 	def test_playVideo_should_handle_over_18_videos(self):
-                sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-		player = sys.modules[ "__main__" ].player
-		player.playVideo({ "videoid": "fOdNOtS8ZIs", "no_embed": "true"})
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+
+		self.navigation.executeAction({"action":"play_video", "videoid": "fOdNOtS8ZIs", "no_embed": "true"})
 
 		args = sys.modules[ "__main__" ].xbmcplugin.setResolvedUrl.call_args_list
 		print "Args: " + repr(args)
@@ -36,9 +35,9 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		assert(args[0][1]["succeeded"] == True)
 
 	def test_playVideo_should_handle_not_embeddable_over_18(self): # Currently this is just not embeddable
-                sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-		player = sys.modules[ "__main__" ].player
-		player.playVideo({ "videoid": "3PjTEO948Lo"})
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		
+		self.navigation.executeAction({"action":"play_video", "videoid": "3PjTEO948Lo"})
 
 		args = sys.modules[ "__main__" ].xbmcplugin.setResolvedUrl.call_args_list
 		print "Args: " + repr(args)
@@ -52,9 +51,9 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 
 
 	def test_playVideo_should_handle_rtmpe_vidoes(self):
-                sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-		player = sys.modules[ "__main__" ].player
-		player.playVideo({ "videoid": "8wxOVn99FTE"})
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+
+		self.navigation.executeAction({"action":"play_video", "videoid": "8wxOVn99FTE"})
 
 		args = sys.modules[ "__main__" ].xbmcplugin.setResolvedUrl.call_args_list
 		print "Args: " + repr(args)
@@ -67,17 +66,17 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		assert(args[0][1]["succeeded"] == True)
 
 	def test_playVideo_should_handle_live_vidoes(self):
-                sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-		player = sys.modules[ "__main__" ].player
-		player.playVideo({ "videoid": "JpYHuK45We0"})
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+				
+		self.navigation.executeAction({"action":"play_video", "videoid": "JpYHuK45We0"})
 
 		args = sys.modules[ "__main__" ].xbmcplugin.setResolvedUrl.call_args_list
-                if sys.modules[ "__main__"].xbmc.executebuiltin.call_count == 0:
+		if sys.modules[ "__main__"].xbmc.executebuiltin.call_count == 0:
 			print "Args: " + repr(args)
 			print repr(args[0][1].has_key("listitem"))
 			print repr(args[0][1]["handle"] == -1)
 			print repr(args[0][1]["succeeded"] == True)
-
+	
 			assert(args[0][1].has_key("listitem"))
 			assert(args[0][1]["handle"] == -1)
 			assert(args[0][1]["succeeded"] == True)
