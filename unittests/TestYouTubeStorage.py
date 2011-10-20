50aa901c3279f61db933fa5f8026374c91abd919
Tobias Ussing <develop@tobiasussing.dk>
2011-10-21 00:48:25 +0200

    Disable io.open unittest
    --HG--
    branch : testing-framework

--- unittests/TestYouTubeStorage.py
+++ unittests/TestYouTubeStorage.py
@@ -34,7 +34,7 @@ class TestYouTubeStorage(BaseTestCase.BaseTestCase):
 		
 		storage.getStoredSearches.assert_called_with({"store":"somestore"})
 
-	def test_openFile_should_call_io_open(self):
+	def ttest_openFile_should_call_io_open(self): # We no longer use io.
 		patcher = patch("io.open")
 		patcher.start()
 		import io
