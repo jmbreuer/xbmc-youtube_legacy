dbea67c72506dfd5772bd1bd6990b0eba063a8f5
Tobias Ussing <develop@tobiasussing.dk>
2011-10-20 10:34:03 +0200

    Update
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeUserFeeds.py
+++ integrationtests/TestYouTubeUserFeeds.py
@@ -1,10 +1,9 @@
 import BaseTestCase
-import nose
-
+import nose, sys
 
 class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
-	
-	def test_plugin_should_list_user_favorites_video_list_correctly(self):
+       	def test_plugin_should_list_user_favorites_video_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"favorites", "login":"true", "path":"/root/favorites"})
 		
@@ -15,6 +14,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_user_favorites_video_list_page_2_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"favorites", "login":"true", "page":"2","path":"/root/favorites"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -24,6 +24,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_user_playlists_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"favorites", "login":"true","path":"/root/playlists"})
 		
@@ -33,6 +34,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("playlist")
 	
 	def test_plugin_should_list_user_playlists_page_2_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"favorites", "login":"true", "page":"1","path":"/root/playlists"})
 		
@@ -42,6 +44,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("playlist")
 	
 	def test_plugin_should_list_user_uploads_videos_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"uploads", 'login':'true', "path":"/root/uploads"})
 		
@@ -52,6 +55,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 		
 	def test_plugin_should_list_user_uploads_videos_list_page_2_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"uploads", 'login':'true', "page":"1", "path":"/root/uploads"})
 		
@@ -62,6 +66,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 
 	def test_plugin_should_list_user_watch_later_video_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"feed":"watch_later", 'login':'true', "path":"/root/watch_later"})
 		
@@ -73,7 +78,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("playlist_entry_id")
 		
 	def test_plugin_should_list_user_watch_later_video_list_page_2_correctly(self):
-
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"watch_later", 'login':'true', "page":"1", "path":"/root/watch_later"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -84,6 +89,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("playlist_entry_id")
 	
 	def test_plugin_should_list_user_contacts_folder_list_correctly_(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"feed":"contacts", 'login':'true', "path":"/root/contacts/smokey"})
 		
@@ -94,6 +100,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("options")
 
 	def test_plugin_should_list_user_contact_folder_list_correctly_(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"options":"contact", "Tobuscus" 'login':'true', "path":"/root/contacts/smokey"})
 		
@@ -103,6 +110,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("contact")
 		
 	def test_plugin_should_list_user_subscriptions_folder_list_correctly_(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"subscriptions", 'login':'true', "path":"/root/subscriptions/something/smokey"})
 		
@@ -111,7 +119,8 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_items_contain("playlist")
 		
-	def test_plugin_should_list_newsubscriptions_video_list_correctly(self):
+	def test_plugin_should_list_newsubscriptions_video_list_correctly(self):	
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"newsubscriptions", "login":"true", "path":"/root/subscriptions/new"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -121,6 +130,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_subscription_favorties_video_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"subscription_favorites", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -130,6 +140,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 
 	def test_plugin_should_list_subscription_favorties_video_list_page_2_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"subscription_favorites", "channel":"Chuggaconroy", "page":"1", "path":"/root/subscriptions"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -139,6 +150,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_subscription_playlist_folder_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"subscription_playlists", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -147,6 +159,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("playlist")
 	
 	def test_plugin_should_list_subscription_uploads_video_list_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"subscription_uploads", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -156,6 +169,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_subscription_uploads_video_list_page_2_correctly(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"feed":"subscription_uploads", "channel":"Chuggaconroy", "page":"1", "path":"/root/subscriptions"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
