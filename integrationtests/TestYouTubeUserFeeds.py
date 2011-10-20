1841a2cf9c10603300284e42411a3175edbbb885
HenrikDK <none@none>
2011-10-20 08:48:02 +0200

    more tests of feeds
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeUserFeeds.py
+++ integrationtests/TestYouTubeUserFeeds.py
@@ -90,16 +90,25 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_folder_list()
-		self.assert_directory_items_contain("playlist")
+		self.assert_directory_items_contain("contact")
 		
 	def test_plugin_should_list_user_subscriptions_folder_list_correctly_(self):
 		
-		self.navigation.listMenu({"feed":"subscriptions", 'login':'true', "path":"/root/subscriptions"})
+		self.navigation.listMenu({"feed":"subscriptions", 'login':'true', "path":"/root/subscriptions/something/smokey"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_items_contain("playlist")
+		
+	def test_plugin_should_list_newsubscriptions_video_list_correctly(self):
+		self.navigation.listMenu({"feed":"newsubscriptions", "login":"true", "path":"/root/subscriptions/new"})
+		
+		self.assert_directory_count_greater_than_or_equals(10)
+		self.assert_directory_count_less_than_or_equals(51)
+		self.assert_directory_is_a_video_list()
+		self.assert_directory_contains_only_unique_video_items()
+		self.assert_directory_items_should_have_external_thumbnails()
 	
 	def test_plugin_should_list_subscription_favorties_video_list_correctly(self):
 		self.navigation.listMenu({"feed":"subscription_favorites", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
@@ -118,7 +127,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
-				
+	
 	def test_plugin_should_list_subscription_playlist_folder_list_correctly(self):
 		self.navigation.listMenu({"feed":"subscription_playlists", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
 		
@@ -126,7 +135,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_count_less_than_or_equals(51)
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_items_contain("playlist")
-		
+	
 	def test_plugin_should_list_subscription_uploads_video_list_correctly(self):
 		self.navigation.listMenu({"feed":"subscription_uploads", "channel":"Chuggaconroy", "path":"/root/subscriptions"})
 		
@@ -135,7 +144,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_video_list()
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
-
+	
 	def test_plugin_should_list_subscription_uploads_video_list_page_2_correctly(self):
 		self.navigation.listMenu({"feed":"subscription_uploads", "channel":"Chuggaconroy", "page":"1", "path":"/root/subscriptions"})
 		
