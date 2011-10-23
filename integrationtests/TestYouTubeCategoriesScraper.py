b81a71142f78b48d0e8d43deebc8fb4e3b073993
HenrikDK <none@none>
2011-10-23 14:02:22 +0200

    fixed category scraper
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeCategoriesScraper.py
+++ integrationtests/TestYouTubeCategoriesScraper.py
@@ -4,7 +4,7 @@ import nose
 
 class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 	
-	def ttest_plugin_should_scrape_categories_folder_list_correctly(self):
+	def test_plugin_should_scrape_categories_folder_list_correctly(self):
 		self.navigation.listMenu({"scraper":"categories", "folder":"true", "path":"/root/explore/categories"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
@@ -13,8 +13,8 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_contain("category")
 		
 				
-	def tvtest_plugin_should_scrape_category_video_list_correctly(self):
-		self.navigation.listMenu({"scraper":"categories", 'category':'cars', "path":"/root/explore/categories"})
+	def test_plugin_should_scrape_category_video_list_correctly(self):
+		self.navigation.listMenu({"scraper":"categories", 'category':'/categories?c=26', "path":"/root/explore/categories"})
 		
 		self.assert_directory_count_greater_than_or_equals(10)
 		self.assert_directory_count_less_than_or_equals(51)
@@ -22,5 +22,14 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 	
+	def test_plugin_should_scrape_category_page_2_video_list_correctly(self):
+		self.navigation.listMenu({"scraper":"categories", 'category':'/categories?c=26', "page":"1", "path":"/root/explore/categories"})
+		
+		self.assert_directory_count_greater_than_or_equals(10)
+		self.assert_directory_count_less_than_or_equals(51)
+		self.assert_directory_is_a_video_list()
+		self.assert_directory_contains_only_unique_video_items()
+		self.assert_directory_items_should_have_external_thumbnails()
+
 if __name__ == "__main__":
 	nose.runmodule()
