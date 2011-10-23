bacd4c3f9ec8f6ec2a14b61f2e04753ec023a6a2
Tobias Ussing <develop@tobiasussing.dk>
2011-10-23 19:54:51 +0200

    Fixed public feeds
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubePublicFeeds.py
+++ integrationtests/TestYouTubePublicFeeds.py
@@ -7,14 +7,12 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 
                 self.assert_directory_count_greater_than_or_equals(10)
                 self.assert_directory_count_less_than_or_equals(51)
-                self.assert_directory_is_a_folder_list()
-                self.assert_directory_items_should_have_thumbnails()
+                self.assert_directory_is_a_video_list()
+		self.assert_directory_items_should_have_external_thumbnails()
                 self.assert_directory_items_contain("category")
                 #assert(False)
 	
-	def ttest_plugin_should_list_search_video_list_correctly(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
-		
+	def test_plugin_should_list_search_video_list_correctly(self):
 		self.navigation.listMenu({"feed":"search", "search":"Star Craft 2", "path":"/root/favorites"})
 		
 		self.assert_directory_count_greater_than_or_equals(5)
@@ -23,10 +21,8 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 	
-	def ttest_plugin_should_list_related_video_list_correctly(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
-		
-		self.navigation.listMenu({"feed":"related", "videoid":"", "path":"/root/favorites"})
+	def test_plugin_should_list_related_video_list_correctly(self):
+		self.navigation.listMenu({"feed":"related", "videoid":"byv-wpqDydI", "path":"/root/favorites"})
 		
 		self.assert_directory_count_greater_than_or_equals(5)
 		self.assert_directory_count_less_than_or_equals(51)
