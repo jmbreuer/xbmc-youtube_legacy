200acd9efb394ec8596f896653f4ec48c23f5c59
HenrikDK <none@none>
2011-10-25 20:59:45 +0200

    more test of playlists
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubePlaylists.py
+++ integrationtests/TestYouTubePlaylists.py
@@ -3,12 +3,15 @@ import nose, sys
 
 class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 	
-	def ttest_plugin_should_queue_playlist_and_start_playback_if_user_select_play_all_in_playlist(self):
+	def test_plugin_should_queue_playlist_and_start_playback_if_user_select_play_all_in_playlist(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
 		
-		self.navigation.executeAction({"action":"play_all", "videoid": "54VJWHL2K3I","playlist":"some_playlist"})
-
-		assert(False)
+		self.navigation.executeAction({"action":"play_all", "videoid": "54VJWHL2K3I","playlist":"E3E0C28746217FA6"})
+		
+		self.assert_playlist_count_greater_than_or_equals(30)
+		self.assert_directory_items_should_have_thumbnails()
+		self.assert_playlist_contains_only_unique_video_items()
+		self.assert_playlist_videos_contain("54VJWHL2K3I")
 	
 	def ttest_plugin_should_queue_playlist_and_start_playback_if_user_select_play_all_outside_playlist(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
@@ -44,6 +47,6 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.navigation.executeAction({"action":"play_all", "scraper":"music_top100", "videoid":"some_id"})
 		
 		assert(False)
-
+	
 if __name__ == "__main__":
 	nose.runmodule()
