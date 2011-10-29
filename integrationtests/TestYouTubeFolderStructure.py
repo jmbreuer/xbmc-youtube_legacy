64642867d83def87e2ce198ac971e6d7edf54d30
HenrikDK <none@none>
2011-10-29 12:40:03 +0200

    tested more of folder structure
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeFolderStructure.py
+++ integrationtests/TestYouTubeFolderStructure.py
@@ -22,6 +22,15 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_count_greater_than_or_equals(3)
 		self.assert_directory_count_less_than_or_equals(10)
 		self.assert_directory_is_a_folder_list()
+		
+	def test_plugin_should_list_subfolders_when_user_navigates_to_a_folder(self):
+		
+		self.navigation.listMenu({"path":"/root/explore"})
+		
+		#we probably need some better asserts here
+		self.assert_directory_count_greater_than_or_equals(8)
+		self.assert_directory_count_less_than_or_equals(10)
+		self.assert_directory_is_a_folder_list()
 	
 if __name__ == "__main__":
 	nose.runmodule()
