952ed5f4fac0d1c8ff96153b4672e0642aa5f483
HenrikDK <none@none>
2011-10-29 13:00:23 +0200

    testing that playlists work and pagination in playlists work
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubeUserFeeds.py
+++ integrationtests/TestYouTubeUserFeeds.py
@@ -3,7 +3,7 @@ import nose, sys
 
 class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 	
-	def test_plugin_should_list_user_favorites_video_list_correctly(self):
+	def ttest_plugin_should_list_user_favorites_video_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		sys.modules["__main__"].settings.setSetting("perpage","5")
 		
@@ -15,7 +15,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 	
-	def test_plugin_should_list_user_favorites_video_list_page_2_correctly(self):
+	def ttest_plugin_should_list_user_favorites_video_list_page_2_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		sys.modules["__main__"].settings.setSetting("perpage","2")
 
@@ -27,7 +27,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 	
-	def test_plugin_should_list_user_playlists_correctly(self):
+	def ttest_plugin_should_list_user_playlists_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"user_feed":"playlists", "login":"true","path":"/root/playlists", "folder": "true"})
@@ -37,7 +37,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_item_urls_contain("playlist")
 	
-	def test_plugin_should_list_user_uploads_videos_list_correctly(self):
+	def ttest_plugin_should_list_user_uploads_videos_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"uploads", 'login':'true', "path":"/root/uploads"})
@@ -48,7 +48,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 		
-	def test_plugin_should_list_user_uploads_videos_list_page_2_correctly(self):
+	def ttest_plugin_should_list_user_uploads_videos_list_page_2_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		sys.modules["__main__"].settings.setSetting("perpage","0")
 		
@@ -60,7 +60,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 
-	def test_plugin_should_list_user_watch_later_video_list_correctly(self):
+	def ttest_plugin_should_list_user_watch_later_video_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"user_feed":"watch_later", 'login':'true', "path":"/root/watch_later"})
@@ -72,7 +72,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_external_thumbnails()
 		self.assert_directory_item_urls_contain("playlist_entry_id")
 	
-	def test_plugin_should_list_user_recommended_video_list_correctly(self):
+	def ttest_plugin_should_list_user_recommended_video_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"feed":"recommended", 'login':'true', "path":"/root/watch_later"})
@@ -83,7 +83,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 		
-	def test_plugin_should_list_user_watch_later_video_list_page_2_correctly(self):
+	def ttest_plugin_should_list_user_watch_later_video_list_page_2_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		self.navigation.listMenu({"user_feed":"watch_later", 'login':'true', "page":"1", "path":"/root/watch_later"})
 		
@@ -93,8 +93,26 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_contains_only_unique_video_items()
 		self.assert_directory_items_should_have_external_thumbnails()
 		self.assert_directory_item_urls_contain("playlist_entry_id")
+
+	def test_plugin_should_list_user_playlist_video_list_correctly_(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
+
+		self.navigation.listMenu({"user_feed":"playlist", 'login':'true', "path":"/root/playlist/smokey", "playlist":"E3E0C28746217FA6"})
+		
+		self.assert_directory_count_greater_than_or_equals(10)
+		self.assert_directory_count_less_than_or_equals(51)
+		self.assert_directory_is_a_video_list()
 	
-	def test_plugin_should_list_user_contacts_folder_list_correctly_(self):
+	def test_plugin_should_list_user_playlist_video_list_page_2_correctly_(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
+
+		self.navigation.listMenu({"user_feed":"playlist", 'login':'true', "path":"/root/playlist/smokey", "playlist":"E3E0C28746217FA6", "page":"1"})
+		
+		self.assert_directory_count_greater_than_or_equals(10)
+		self.assert_directory_count_less_than_or_equals(51)
+		self.assert_directory_is_a_video_list()
+
+	def ttest_plugin_should_list_user_contacts_folder_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 
 		self.navigation.listMenu({"user_feed":"contacts", 'login':'true', "path":"/root/contacts/smokey", "folder": "true"})
@@ -104,7 +122,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_item_urls_contain("contact")
 
-	def test_plugin_should_list_user_subscriptions_folder_list_correctly_(self):
+	def ttest_plugin_should_list_user_subscriptions_folder_list_correctly_(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"user_feed":"subscriptions", 'login':'true', "folder":"true", "path":"/root/subscriptions/something/smokey"})
@@ -114,7 +132,7 @@ class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
 		self.assert_directory_is_a_folder_list()
 		self.assert_directory_item_urls_contain("channel")
 		
-	def test_plugin_should_list_newsubscriptions_video_list_correctly(self):	
+	def ttest_plugin_should_list_newsubscriptions_video_list_correctly(self):	
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.listMenu({"feed":"newsubscriptions", "login":"true", "path":"/root/subscriptions/new"})
