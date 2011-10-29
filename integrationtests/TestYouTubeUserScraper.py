973df3318b0119dfe841fa5b721c74f9f9fb164f
HenrikDK <none@none>
2011-10-30 00:28:46 +0200

    deleted an old unused test

--- integrationtests/TestYouTubeUserScraper.py
+++ integrationtests/TestYouTubeUserScraper.py
@@ -2,18 +2,7 @@ import BaseTestCase
 import nose, sys
 
 class TestYouTubeMusicScraper(BaseTestCase.BaseTestCase):
-	
-	def ttest_plugin_should_scrape_user_recommendations_video_list_correctly(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
-		self.navigation.listMenu({"scraper":"recommended", "login":"true", "path":"/root/recommended"})
-		
-		self.assert_directory_count_greater_than_or_equals(10)
-		self.assert_directory_count_less_than_or_equals(51)
-		self.assert_directory_is_a_video_list()
-		self.assert_directory_contains_only_unique_video_items()
-		self.assert_directory_items_should_have_external_thumbnails()
-	
 	def test_plugin_should_scrape_liked_videos_list_correctly(self):
 		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
