ebda1ef643c9001d91564ef72b3cbaed68bc5914
Tobias Ussing <develop@tobiasussing.dk>
2011-10-31 13:41:15 +0100

    Change settings object to logged-in for all test(sans login). Enabled tests in TestYouTubePlaylists.py
    --HG--
    branch : testing-framework

--- integrationtests/TestYouTubePlaylists.py
+++ integrationtests/TestYouTubePlaylists.py
@@ -4,7 +4,7 @@ import nose, sys
 class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 	
 	def ttest_plugin__should_queue_playlist_and_start_playback_if_user_selects_play_all_in_playlist(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "videoid": "Q7GVSx7yMaA","playlist":"E3E0C28746217FA6"})
 		
@@ -15,7 +15,7 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_playlist_videos_contain("Q7GVSx7yMaA")
 	
 	def ttest_plugin__should_queue_playlist_and_start_playback_if_user_selects_play_all_outside_playlist(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "playlist":"E3E0C28746217FA6"})
 		
@@ -23,8 +23,8 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_thumbnails()
 		self.assert_playlist_contains_only_unique_video_items()
 		
-	def ttest_plugin__should_queue_disco_search_and_start_playback_if_user_selects_play_all_outside_disco_search(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+	def test_plugin__should_queue_disco_search_and_start_playback_if_user_selects_play_all_outside_disco_search(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "scraper":"search_disco","search":"Linkin+Park"})
 		
@@ -32,8 +32,8 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_thumbnails()
 		self.assert_playlist_contains_only_unique_video_items()
 	
-	def ttest_plugin__should_queue_disco_search_and_start_playback_if_user_selects_play_all_in_disco_search(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+	def test_plugin__should_queue_disco_search_and_start_playback_if_user_selects_play_all_in_disco_search(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "scraper":"search_disco","search":"Linkin+Park", "videoid":"8sgycukafqQ"})
 		
@@ -43,8 +43,8 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_playlist_videos_contain("8sgycukafqQ")
 		self.assert_playlist_videos_does_not_contain("ysSxxIqKNN0")
 	
-	def ttest_plugin__should_queue_youtube_top_100_and_start_playback_if_user_selects_play_all_outside_list(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+	def test_plugin__should_queue_youtube_top_100_and_start_playback_if_user_selects_play_all_outside_list(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "scraper":"music_top100"})
 		
@@ -52,8 +52,8 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_thumbnails()
 		self.assert_playlist_contains_only_unique_video_items()
 		
-	def ttest_plugin__should_queue_user_watch_later_feed_if_user_selects_play_all_outside_list(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+	def test_plugin__should_queue_user_watch_later_feed_if_user_selects_play_all_outside_list(self):
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "user_feed":"watch_later"})
 		
@@ -62,7 +62,7 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_playlist_contains_only_unique_video_items()
 		
 	def ttest_plugin__should_queue_user_new_subscriptions_feed_if_user_selects_play_all_on_external_user_outside_list(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "contact":"TobiasTheViking", "user_feed":"newsubscriptions"})
 		
@@ -71,7 +71,7 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_playlist_contains_only_unique_video_items()
 	
 	def test_plugin__should_queue_users_liked_videos_if_user_selects_play_all_outside_list(self):
-		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
+		sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
 		
 		self.navigation.executeAction({"action":"play_all", "scraper":"liked_videos", "login":"true"})
 		
@@ -79,7 +79,7 @@ class TestYouTubePlayer(BaseTestCase.BaseTestCase):
 		self.assert_directory_items_should_have_thumbnails()
 		self.assert_playlist_contains_only_unique_video_items()
 		
-	def ttest_plugin__should_queue_artist_video_list_if_user_selects_play_all_outside_list(self):
+	def test_plugin__should_queue_artist_video_list_if_user_selects_play_all_outside_list(self):
 		
 		self.navigation.executeAction({"action":"play_all", "scraper":"music_artist", "artist":"GxdCwVVULXewLzmGzHQKeUHShvQYOHQH"})
 		
