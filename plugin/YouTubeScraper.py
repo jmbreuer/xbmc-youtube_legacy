063fc43e24a7a127bc8cd7d6e7eebd80bf13bf6d
HenrikDK <none@none>
2011-11-14 22:18:37 +0100

    removed unused code for categories scraper

--- plugin/YouTubeScraper.py
+++ plugin/YouTubeScraper.py
@@ -125,47 +125,6 @@ class YouTubeScraper():
 		del params["page"]
 		self.common.log("Done")
 		return (items, result["status"])
-	
-#=================================== Categories  ============================================
-	def scrapeCategoriesGrid(self, params = {}):
-		get = params.get
-		self.common.log("")
-		items = []
-		next = True
-		page = 0 
-		
-		url = self.createUrl(params)
-		result = self.core._fetchPage({"link":url})
-
-		while next:
-			params["page"] = str(page)
-			url = self.createUrl(params)
-			result = self.core._fetchPage({"link":url})
-			
-			page += 1
-			
-			next = False
-			if result["status"] == 200:
-				pagination = self.common.parseDOM(result["content"], "div", { "class": "yt-uix-pager"})
-				if (len(pagination) > 0):
-					tmp = str(pagination)
-					if (tmp.find("Next") > 0):
-						next = True
-				
-				videos = self.common.parseDOM(result["content"], "div", { "id": "browse-video-data"})
-				if len(videos) == 0:
-					videos = self.common.parseDOM(result["content"], "div", attrs= { "class": "most-viewed-list paginated"})
-			
-				if len(videos) == 0: # Videos from education.
-					videos = self.common.parseDOM(result["content"], "div", attrs= { "class": "ytg-fl browse-content"})
-				videos = self.common.parseDOM(videos, "a", attrs = { "class": "ux-thumb-wrap " } , ret = "href")
-				
-				videos = self.utils.extractVID(videos)
-				items = items + videos
-		
-		self.common.log("Done")
-		return (items, result["status"])
-
 		
 #=================================== Music  ============================================
 	def scrapeMusicCategories(self, params = {}):
@@ -905,11 +864,7 @@ class YouTubeScraper():
 				params["batch"] = "true"
 				del params["folder"] 
 				function = self.scrapeEducationVideos
-			
-		if get("scraper") == "categories" and get("category"):
-			params["batch"] = "true"
-			function = self.scrapeCategoriesGrid
-		
+				
 		if (get("scraper") in ['current_trailers','game_trailers','popular_game_trailers','popular_trailers','trailers','upcoming_game_trailers','upcoming_trailers']):
 			params["batch"] = "thumbnails"
 			function = self.scrapeTrailersGridFormat
