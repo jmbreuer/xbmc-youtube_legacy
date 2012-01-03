d4bbb42384baf154c88d74ad6f956c706904d173
Tobias Ussing <develop@tobiasussing.dk>
2012-01-03 15:17:31 +0100

    Something stupid: making load_strings fubar

--- integrationtests/xbmcSettings.py
+++ integrationtests/xbmcSettings.py
@@ -1,52 +1,54 @@
 import xml.dom.minidom as minidom
-import io, inspect
+import io
+import inspect
+
 
 class xbmcSettings():
-	
-	def __init__(self):
-		self.settingsString = {}
-		self.path = ""
-	
-	def load_strings(self, path = "./resources/settings.xml"):
-		print " *** *** loading settings strings *** ***"
-		self.path = path
-		file = io.open(path).read()
-		self.dom = minidom.parseString(file);
-		self.strings = self.dom.getElementsByTagName("setting")
-		
-		for string in self.strings:
-			self.settingsString[string.getAttribute("id")] = string.getAttribute("value")		
+    def __init__(self):
+        self.settingsString = {}
+        self.path = ""
 
-	def __call__(self, id = "", value = ""):
-		
-		if not self.settingsString:
-			self.load_strings()
-		
-		if value:
-			self.settingsString[id] = value
-			if self.path.find("settings-logged-in") > -1: # This only updates. No insert!
-				updated = False
-				for string in self.strings:
-					if string.getAttribute("id") == id:
-						string.setAttribute("value", value)
-						updated = True
-				f = open(self.path, 'w')
-				self.dom.writexml(f)
-				f.close()
-		
-		elif id in self.settingsString:
-			return self.settingsString[id]
-		
-		return ""
+    def load_strings(self, path="./resources/settings.xml"):
+        print " *** *** loading settings strings *** ***"
+        return None
+        self.path = path
+        file = io.open(path).read()
+        self.dom = minidom.parseString(file)
+        self.strings = self.dom.getElementsByTagName("setting")
 
-	def __getattr__(self, name):
-		return self
+        for string in self.strings:
+            self.settingsString[string.getAttribute("id")] = string.getAttribute("value")
+
+    def __call__(self, id="", value=""):
+
+        if not self.settingsString:
+            self.load_strings()
+
+        if value:
+            self.settingsString[id] = value
+            if self.path.find("settings-logged-in") > -1:  # This only updates. No insert!
+                updated = False
+                for string in self.strings:
+                    if string.getAttribute("id") == id:
+                        string.setAttribute("value", value)
+                        updated = True
+                f = open(self.path, 'w')
+                self.dom.writexml(f)
+                f.close()
+
+        elif id in self.settingsString:
+            return self.settingsString[id]
+
+        return ""
+
+    def __getattr__(self, name):
+        return self
 
 if __name__ == "__main__":
-	x1 = xbmcSettings()
-	print "x1: " + x1.getSetting("downloads")
-	print "setting x1 = funkytown"
-	x1.setSetting("downloads", "funkytown" )
-	x2 = xbmcSettings()
-	print "x2: " + x2.getSetting("downloads")
-	print "x1: " + x1.getSetting("downloads")
+    x1 = xbmcSettings()
+    print "x1: " + x1.getSetting("downloads")
+    print "setting x1 = funkytown"
+    x1.setSetting("downloads", "funkytown" )
+    x2 = xbmcSettings()
+    print "x2: " + x2.getSetting("downloads")
+    print "x1: " + x1.getSetting("downloads")
