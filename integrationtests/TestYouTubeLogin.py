773cdfca9ed774b96387c991443438799cfe500d
HenrikDK <none@none>
2012-01-03 23:50:01 +0100

    fixed integration test of 2 factor login

--- integrationtests/TestYouTubeLogin.py
+++ integrationtests/TestYouTubeLogin.py
@@ -58,34 +58,34 @@ class TestYouTubeLogin(BaseTestCase.BaseTestCase):
         import pyotp
         self.totp = pyotp.TOTP("fbfkkk27ffmaihzg")
         self.lastpin = False
-        
+
         sys.modules["__main__"].settings.load_strings("./resources/2factor-login-settings.xml")
-        sys.modules["__main__"].xbmcgui = Mock(Name="myfavoriteMock")
-        sys.modules["__main__"].xbmcgui.Dialog().numeric().side_effect = self.generatePin
-        
-        self.intializePlugin()
-        tmp = sys.modules["__main__"].common.getUserInputNumbers("testing")
-        print repr(tmp)
-        assert(False)
-#        assert(sys.modules["__main__"].settings.getSetting("nick") == "")
-#        assert(sys.modules["__main__"].settings.getSetting("auth") == "")
-#        assert(sys.modules["__main__"].settings.getSetting("oauth2_access_token") == "")
-#
-#        print "nick: " + sys.modules["__main__"].settings.getSetting("nick")
-#        print "username: " + sys.modules["__main__"].settings.getSetting("username")
-#        print "pass: " + sys.modules["__main__"].settings.getSetting("user_password")
-#        print "oauth2_access_token: " + sys.modules["__main__"].settings.getSetting("oauth2_access_token")
-#
-#        self.navigation.executeAction({"action": "settings"})
-#
-#        nick = sys.modules["__main__"].settings.getSetting("nick")
-#        oauth2_access_token = sys.modules["__main__"].settings.getSetting("oauth2_access_token")
-#        print "nick: " + nick + " - " + str(len(nick))
-#        print "username: " + sys.modules["__main__"].settings.getSetting("username")
-#        print "pass: " + sys.modules["__main__"].settings.getSetting("user_password")
-#        print "oauth2_access_token: " + oauth2_access_token + " - " + str(len(oauth2_access_token))
-#        assert(len(nick.strip()) > 1 )
-#        assert(len(oauth2_access_token) > 40)
+        tmp = sys.modules["__main__"].xbmcgui.Dialog()
+        tmp.numeric.side_effect = self.generatePin
+        #self.intializePlugin()
+        #tmp = sys.modules["__main__"].common.getUserInputNumbers("testing")
+
+        #print repr(tmp)
+        #assert(False)
+        assert(sys.modules["__main__"].settings.getSetting("nick") == "")
+        assert(sys.modules["__main__"].settings.getSetting("auth") == "")
+        assert(sys.modules["__main__"].settings.getSetting("oauth2_access_token") == "")
+
+        print "nick: " + sys.modules["__main__"].settings.getSetting("nick")
+        print "username: " + sys.modules["__main__"].settings.getSetting("username")
+        print "pass: " + sys.modules["__main__"].settings.getSetting("user_password")
+        print "oauth2_access_token: " + sys.modules["__main__"].settings.getSetting("oauth2_access_token")
+
+        self.navigation.executeAction({"action": "settings"})
+
+        nick = sys.modules["__main__"].settings.getSetting("nick")
+        oauth2_access_token = sys.modules["__main__"].settings.getSetting("oauth2_access_token")
+        print "nick: " + nick + " - " + str(len(nick))
+        print "username: " + sys.modules["__main__"].settings.getSetting("username")
+        print "pass: " + sys.modules["__main__"].settings.getSetting("user_password")
+        print "oauth2_access_token: " + oauth2_access_token + " - " + str(len(oauth2_access_token))
+        assert(len(nick.strip()) > 1 )
+        assert(len(oauth2_access_token) > 40)
 
     def ttest_plugin_should_perform_googleplus_login_correctly(self):
         sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-plus.xml")
