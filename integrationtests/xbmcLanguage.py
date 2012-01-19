fbb28404745b7dff07c9e0895eb8224c4c237968
Tobias Ussing <develop@tobiasussing.dk>
2012-01-19 19:29:17 +0100

    Cleanup

--- integrationtests/xbmcLanguage.py
+++ integrationtests/xbmcLanguage.py
@@ -1,39 +1,40 @@
 import xml.dom.minidom as minidom
 import io
 
+
 class xbmcLanguage:
-	"""
-	Borg singleton config object
-	"""
-	_we_are_one = {}
-	_languageString = {}
-	
-	def __init__(self):
-		self.__dict__ = self._we_are_one
+    """
+    Borg singleton config object
+    """
+    _we_are_one = {}
+    _languageString = {}
 
-	def _load_strings(self):
-		print " *** *** loading language strings  *** ***"
-		file = io.open("./resources/strings.xml").read()
-		
-		dom = minidom.parseString(file);
-		strings = dom.getElementsByTagName("string")
-		
-		for string in strings:
-			if string.firstChild:
-				self._languageString[int(string.getAttribute("id"))] = string.firstChild.data
+    def __init__(self):
+        self.__dict__ = self._we_are_one
 
-	def __call__(self, id):
-		if not self._languageString:
-			self._load_strings()
-		
-		if id in self._languageString:
-			return self._languageString[id]
-		return ""
+    def _load_strings(self):
+        print " *** *** loading language strings  *** ***"
+        file = io.open("./resources/strings.xml").read()
+
+        dom = minidom.parseString(file)
+        strings = dom.getElementsByTagName("string")
+
+        for string in strings:
+            if string.firstChild:
+                self._languageString[int(string.getAttribute("id"))] = string.firstChild.data
+
+    def __call__(self, id):
+        if not self._languageString:
+            self._load_strings()
+
+        if id in self._languageString:
+            return self._languageString[id]
+        return ""
 
 if __name__ == "__main__":
-	x = xbmcLanguage()
-	print x(30230)
-	print x(30232)
-	x2 = xbmcLanguage()
-	print x2(30232)
-	print x2(30230)		
+    x = xbmcLanguage()
+    print x(30230)
+    print x(30232)
+    x2 = xbmcLanguage()
+    print x2(30232)
+    print x2(30230)
