a14f9ab195662ab058c6b0cecc0c80cd781f61b5
Tobias Ussing <develop@tobiasussing.dk>
2012-02-05 01:34:18 +0100

    Test download filesize

--- integrationtests/TestYouTubeDownloader.py
+++ integrationtests/TestYouTubeDownloader.py
@@ -18,6 +18,7 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
 
         self.navigation.executeAction({"action": "download", "videoid": "54VJWHL2K3I", "async": "false"})
         assert(os.path.exists('./tmp/Roll a D6-[54VJWHL2K3I].mp4'))
+        assert(os.path.getsize('./tmp/Roll a D6-[54VJWHL2K3I].mp4') > 100)
 
     def test_plugin_should_download_agerestricted_over_18_videos(self):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
@@ -31,6 +32,7 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
         self.navigation.executeAction({"action": "download", "videoid": "fOdNOtS8ZIs", "no_embed": "true", "async": "false"})
 
         assert(os.path.exists("./tmp/נלה מהיפה והחנון בסטריפ צ'אט בקליפ של חובבי ציון-[fOdNOtS8ZIs].mp4"))
+        assert(os.path.getsize("./tmp/נלה מהיפה והחנון בסטריפ צ'אט בקליפ של חובבי ציון-[fOdNOtS8ZIs].mp4") > 100)
 
     def test_plugin_should_download_with_subtitles_when_available(self):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
@@ -45,6 +47,8 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
 
         assert(os.path.exists('./tmp/Morning Dew — a bad lip reading of Bruno Mars, feat. Lady Gaga and Jay-Z-[bUcszN8jRB8].mp4'))
         assert(os.path.exists('./tmp/Morning Dew  a bad lip reading of Bruno Mars, feat. Lady Gaga and Jay-Z-[bUcszN8jRB8].ssa'))
+        assert(os.path.getsize('./tmp/Morning Dew — a bad lip reading of Bruno Mars, feat. Lady Gaga and Jay-Z-[bUcszN8jRB8].mp4') > 100)
+        assert(os.path.getsize('./tmp/Morning Dew  a bad lip reading of Bruno Mars, feat. Lady Gaga and Jay-Z-[bUcszN8jRB8].ssa') > 100)
 
 if __name__ == "__main__":
     nose.runmodule()
