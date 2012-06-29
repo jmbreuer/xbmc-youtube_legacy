2689ba55fd8a8ff54dca4edc85686077626308c2
HenrikDK <none@none>
2012-06-30 00:56:16 +0200

    fixed broken scraper test

--- integrationtests/TestYouTubeShowsScraper.py
+++ integrationtests/TestYouTubeShowsScraper.py
@@ -23,13 +23,13 @@ class TestYouTubeShowsScraper(BaseTestCase.BaseTestCase):
         self.assert_directory_item_urls_contain("show")
         self.assert_directory_items_should_have_external_thumbnails()
 
-    def test_plugin_should_scrape_show_episode_video_list_correctly(self):
+    def test_plugin_should_scrape_show_single_season_folder_list_correctly(self):
         self.navigation.listMenu({"scraper": "shows", "path": "/root/explore/trailers/current", "show": "blackboxtv?feature=sh_b_dr_4_3"})
 
-        self.assert_directory_count_greater_than_or_equals(11)
-        self.assert_directory_count_less_than_or_equals(51)
-        self.assert_directory_is_a_video_list()
-        self.assert_directory_contains_almost_only_unique_video_items()
+        self.assert_directory_count_greater_than_or_equals(1)
+        self.assert_directory_count_less_than_or_equals(1)
+        self.assert_directory_is_a_folder_list()
+        self.assert_directory_item_urls_contain("season")
         self.assert_directory_items_should_have_external_thumbnails()
 
     def test_plugin_should_scrape_show_season_folder_list_correctly(self):
@@ -50,7 +50,7 @@ class TestYouTubeShowsScraper(BaseTestCase.BaseTestCase):
         self.assert_directory_contains_almost_only_unique_video_items()
         self.assert_directory_items_should_have_external_thumbnails()
 
-    def ttest_plugin_should_scrape_show_season_episode_video_list_page_2_correctly(self):  # Epic failure
+    def test_plugin_should_scrape_show_season_episode_video_list_page_2_correctly(self):  # Epic failure
         self.navigation.listMenu({"scraper": "shows", "path": "/root/explore/trailers/current", "show": "minecraft?feature=sh_gm_show_1_1", "season": "ELEskn0lrWVDA"})
 
         self.assert_directory_count_greater_than_or_equals(10)
