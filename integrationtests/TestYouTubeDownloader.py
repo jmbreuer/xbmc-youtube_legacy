d00dc60580b4823857f6cbca5ec2630ba13f5003
HenrikDK <none@none>
2012-09-08 23:37:13 +0200

    removed invalid disabled tests

--- integrationtests/TestYouTubeDownloader.py
+++ integrationtests/TestYouTubeDownloader.py
@@ -7,6 +7,7 @@ from mock import Mock
 
 
 class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
+
     def test_plugin_should_download_standard_videos(self):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
         sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
@@ -19,20 +20,6 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
         self.navigation.executeAction({"action": "download", "videoid": "54VJWHL2K3I", "async": "false"})
         assert(os.path.exists('./tmp/Roll a D6-[54VJWHL2K3I].mp4'))
         assert(os.path.getsize('./tmp/Roll a D6-[54VJWHL2K3I].mp4') > 100)
-
-    def ttest_plugin_should_download_agerestricted_over_18_videos(self):
-        sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
-        sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
-        sys.modules["__main__"].downloader._getNextItemFromQueue = Mock()
-        (video, status) = sys.modules["__main__"].player.getVideoObject({"action": "download", "videoid": "fOdNOtS8ZIs"})
-        video["download_path"] = "./tmp/"
-        video["url"] = video["video_url"]
-        sys.modules["__main__"].downloader._getNextItemFromQueue.side_effect = [("נלה מהיפה והחנון בסטריפ צ'אט בקליפ של חובבי ציון-[fOdNOtS8ZIs].mp4", video), {}]
-
-        self.navigation.executeAction({"action": "download", "videoid": "fOdNOtS8ZIs", "no_embed": "true", "async": "false"})
-
-        assert(os.path.exists("./tmp/נלה מהיפה והחנון בסטריפ צ'אט בקליפ של חובבי ציון-[fOdNOtS8ZIs].mp4"))
-        assert(os.path.getsize("./tmp/נלה מהיפה והחנון בסטריפ צ'אט בקליפ של חובבי ציון-[fOdNOtS8ZIs].mp4") > 100)
 
     def ttest_plugin_should_download_with_subtitles_when_available(self):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
