700c8b9aa20c5eb9826b006c61f8e3627ed58840
HenrikDK <none@none>
2012-09-17 22:07:31 +0200

    some refactor

--- integrationtests/TestYouTubeDownloader.py
+++ integrationtests/TestYouTubeDownloader.py
@@ -12,7 +12,7 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
         sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings-logged-in.xml")
         sys.modules["__main__"].downloader._getNextItemFromQueue = Mock()
-        (video, status) = sys.modules["__main__"].player.getVideoObject({"action": "download", "videoid": "54VJWHL2K3I"})
+        (video, status) = sys.modules["__main__"].player.buildVideoObject({"action": "download", "videoid": "54VJWHL2K3I"})
         video["download_path"] = "./tmp/"
         video["url"] = video["video_url"]
         sys.modules["__main__"].downloader._getNextItemFromQueue.side_effect = [("Roll a D6-[54VJWHL2K3I].mp4", video), {}]
@@ -25,7 +25,7 @@ class TestYouTubeDownloader(BaseTestCase.BaseTestCase):
         sys.modules["__main__"].xbmcvfs.rename.side_effect = os.rename
         sys.modules["__main__"].settings.load_strings("./resources/basic-login-settings.xml")
         sys.modules["__main__"].downloader._getNextItemFromQueue = Mock()
-        (video, status) = sys.modules["__main__"].player.getVideoObject({"action": "download", "videoid": "bUcszN8jRB8"})
+        (video, status) = sys.modules["__main__"].player.buildVideoObject({"action": "download", "videoid": "bUcszN8jRB8"})
         video["download_path"] = "./tmp/"
         video["url"] = video["video_url"]
         sys.modules["__main__"].downloader._getNextItemFromQueue.side_effect = [("Morning Dew — a bad lip reading of Bruno Mars, feat. Lady Gaga and Jay-Z-[bUcszN8jRB8].mp4", video), {}]
