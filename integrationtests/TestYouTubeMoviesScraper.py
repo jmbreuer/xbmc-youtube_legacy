764100a7fb981e78ec23e3c6757c0d71c3b4dff3
HenrikDK <none@none>
2012-11-11 18:04:50 +0100

    disabled indian movie trailers

--- integrationtests/TestYouTubeMoviesScraper.py
+++ integrationtests/TestYouTubeMoviesScraper.py
@@ -12,7 +12,7 @@ class TestYouTubeMoviesScraper(BaseTestCase.BaseTestCase):
         self.assert_directory_items_should_have_thumbnails()
         self.assert_directory_item_urls_contain("category")
 
-    def test_plugin_should_scrape_movie_sub_category_listing_correctly(self):
+    def ttest_plugin_should_scrape_movie_sub_category_listing_correctly(self):
         self.navigation.listMenu({"scraper": "movies", "folder": "true", "category": "indian_cinema", "subcategory": "true", "path": "/root/explore/movies"})
 
         self.assert_directory_count_greater_than_or_equals(1)
@@ -30,7 +30,7 @@ class TestYouTubeMoviesScraper(BaseTestCase.BaseTestCase):
         self.assert_directory_items_should_have_poster_thumbnails()
         self.assert_directory_should_have_next_folder()
 
-    def test_plugin_should_scrape_movie_sub_category_video_list_correctly(self):
+    def ttest_plugin_should_scrape_movie_sub_category_video_list_correctly(self):
         self.navigation.listMenu({"scraper": "movies", "category": "indian-cinema?fl=f&l=te&pt=g&st=f", "path": "/root/explore/movies"})
 
         self.assert_directory_count_greater_than_or_equals(10)
@@ -40,17 +40,6 @@ class TestYouTubeMoviesScraper(BaseTestCase.BaseTestCase):
         self.assert_directory_items_should_have_external_thumbnails()
         self.assert_directory_items_should_have_poster_thumbnails()
         self.assert_directory_should_have_next_folder()
-
-    def ttest_plugin_should_scrape_us_rental_movie_listing_correctly(self):
-        self.navigation.listMenu({"scraper": "movies", "path": "/root/explore/movies"})
-
-        assert(False)  # broken until further notice :D
-        self.assert_directory_count_greater_than_or_equals(10)
-        self.assert_directory_count_less_than_or_equals(51)
-        self.assert_directory_is_a_video_list()
-        self.assert_directory_contains_almost_only_unique_video_items()
-        self.assert_directory_items_should_have_thumbnails()
-        self.assert_directory_items_should_have_poster_thumbnails()
 
 if __name__ == "__main__":
     nose.runmodule()
